8a49db614d257cf4fd21c02580a97d0f228d6a68
danielalexander10 <danielalexander2015gmail.com>
2021-06-14 17:36:47 -0600

    [ADD]Barra de Velocidad Animacion

--- 20211SVAC/G13/edd_proyecto/src/Componentes/Paginas/Cola.js
+++ 20211SVAC/G13/edd_proyecto/src/Componentes/Paginas/Cola.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Cola(){
+
+	const [valor,setValor] = useState("");
+	const [rango,setRango] = useState("1");
+
 	return (
 		<>
-			<div>
-				<table className="table table-hover">
-  					<thead>
-  					  <tr>
-  					    <th scope="col"> </th>
-  					    <th scope="col">
-  					    	<input type="text" className="form-control" placeholder="Ingrese Dato" id="InputListaSE"></input>
-  					    </th>
-  					    <th scope="col">
-  					    	<button type="button" className="btn btn-primary">Agregar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-danger">Eliminar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-warning">Actualizar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-dark">Buscar</button>
-  					    </th>
-  					    <th>
-					      <input className="form-control" type="file" id="formFile"></input>
-  					    </th>
-  					    <th>
-  					    	<button type="button" class="btn btn-success">Guardar</button>
-  					    </th>	
-  					  </tr>
-  					</thead>
-  				</table>
-  				
-
+			<div className="row">
+				<div className="col-md-12">
+					<table className="table table-hover"></table>
+				</div>
 			</div>
-			<h1>Cola</h1>
+			<div className="row">
+				<div className="col-md-3" style={{marginLeft: 2 + 'em'}}>
+					<input type="text" className="form-control" placeholder="Ingrese Dato" id="InputCola"></input>
+				</div>
+				<div className="col-md-1">
+					<button type="button" className="btn btn-primary">Agregar</button>
+				</div>
+				<div className="col-md-1">
+					<button type="button" className="btn btn-danger">Eliminar</button>
+				</div>
+				<div className="col-md-1">
+					<button type="button" className="btn btn-warning">Actualizar</button>
+				</div>
+				<div className="col-md-1" style={{marginLeft: 2 + 'em'}}>
+					<button type="button" className="btn btn-dark">Buscar</button>
+				</div>
+				<div className="col-md-3">
+					<input className="form-control" type="file" id="formFile"></input>
+				</div>
+				<div className="col-md-1">
+					<button type="button" class="btn btn-success">Guardar</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-12">
+					<table className="table table-hover"></table>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-2" style={{marginLeft: 2 + 'em'}}>
+					<h5>Rango de Animacion</h5>
+				</div>
+				<div className="col-md-6">
+					<input type="range" className="form-range" min="1" max="10" step="1" defaultValue="1" name="RangoCola" onChange={(e) => setRango(e.target.value)}></input>
+				</div>
+				<div className="col-md-3" style={{marginLeft: 1 + 'em'}}>
+					<h5>Velocidad: x{rango}</h5>
+				</div>
+			</div>
 		</>
 	);
 }
--- 20211SVAC/G13/edd_proyecto/src/Componentes/Paginas/Insercion.js
+++ 20211SVAC/G13/edd_proyecto/src/Componentes/Paginas/Insercion.js
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Insercion(){
+	const [valor,setValor] = useState("");
+	const [rango,setRango] = useState("1");
+
 	return (
 		<>
-			<div>
-				<table className="table table-hover">
-  					<thead>
-  					  <tr>
-  					    <th scope="col"> </th>
-  					    <th scope="col">
-  					    	<input type="text" className="form-control" placeholder="Ingrese Dato" id="InputListaSE"></input>
-  					    </th>
-  					    <th scope="col">
-  					    	<button type="button" className="btn btn-primary">Agregar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-danger">Eliminar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-warning">Actualizar</button>
-  					    </th>
-  					    <th>
-  					    	<button type="button" className="btn btn-dark">Buscar</button>
-  					    </th>
-  					    <th>
-					      <input className="form-control" type="file" id="formFile"></input>
-  					    </th>
-  					    <th>
-  					    	<button type="button" class="btn btn-success">Guardar</button>
-  					    </th>	
-  					  </tr>
-  					</thead>
-  				</table>
-  				
-
+			<div className="row">
+				<div className="col-md-12">
+					<table className="table table-hover"></table>
+				</div>
 			</div>
-			<h1>Insercion</h1>
+			<div className="row">
+				<div className="col-md-6" style={{marginLeft: 2 + 'em'}}>
+					<input className="form-control" type="file" id="formFile"></input>
+				</div>
+				<div className="col-md-1">
+				</div>
+				<div className="col-md-2" style={{marginLeft: 2 + 'em'}}>
+					<button type="button" className="btn btn-primary">Ordenar</button>
+				</div>
+				<div className="col-md-2" style={{marginRight: 2 + 'em'}}>
+					<button type="button" class="btn btn-success">Guardar</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-12">
+					<table className="table table-hover"></table>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-2" style={{marginLeft: 2 + 'em'}}>
+					<h5>Rango de Animacion</h5>
+				</div>
+				<div className="col-md-6">
+					<input type="range" className="form-range" min="1" max="10" step="1" defaultValue="1" name="RangoCola" onChange={(e) => setRango(e.target.value)}></input>
+				</div>
+				<div className="col-md-3" style={{marginLeft: 1 + 'em'}}>
+					<h5>Velocidad: x{rango}</h5>
+				</div>
+			</div>
 		</>
 	);
 }
