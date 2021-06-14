f91b3972149d144a021af22f5523044e2105c441
Moisés <voupi.sfh@gmail.com>
2021-06-13 22:53:00 -0600

    Correción código

--- 20211SVAC/G09/TytusDS/src/app/componentes/estructuraslineales/estructuras-lineales.component.ts
+++ 20211SVAC/G09/TytusDS/src/app/componentes/estructuraslineales/estructuras-lineales.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { GraficasService } from "../../services/graficas/graficas.service";
 import {Router} from '@angular/router';
-//import { HttpClient } from "@angular/common/http";
-=======
-//import { GraficasService } from "../../services/graficas/graficas.service";
 import { HttpClient } from "@angular/common/http";
->>>>>>> 48810f1d1a62e2c19fdbf5f3107a7c0308de8bab
 @Component({
   selector: 'app-estructuras-lineales',
   templateUrl: './estructuras-lineales.component.html',
