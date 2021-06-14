58976221491839a053f08c660614970cebf5d0df
Andrés <feriafansomega@gmail.com>
2021-06-13 21:03:19 -0600

    Comenzando

---
+++ 20211SVAC/G24/ds/simpleList.js
@@ -0,0 +1,69 @@
+
+class node{
+	constructor(data=null){
+		this.data = data;
+		this.next = null;
+	}
+}
+class linkedList{
+	constructor(){
+		this.head = null;
+		this.size = 0;
+	}
+
+	add(elem){
+		var nd = new node(elem);
+		var curr;
+		if (this.head == null){
+			this.head = nd;
+		} else{
+			curr = this.head;
+			while (curr.next){
+				curr = curr.next;
+			}
+			curr.next = nd;
+		}
+		console.log(elem)
+		this.size++;
+	
+	}
+	del(pos){
+		if (pos > 0 && pos >= this.size){
+			return
+		}
+		var curr,prev;
+		curr = this.head;	
+		if (pos === 0){
+			this.head = curr.next;
+		}else{
+			var it=0;
+			while (it<pos){
+				it++;
+				prev = curr;
+				curr = curr.next;
+			}
+			prev.next = curr.next;
+		}
+		this.size--;
+		
+	}
+	update(pos){
+		//pass
+	}
+
+	search(elem){
+		var curr;
+		curr = this.head;
+		var count=1;
+		while (curr!=null){
+			if(curr.data==elem){
+				return count;
+			}
+			count++;
+			curr=curr.next
+		}
+		return;
+	}
+
+
+}
