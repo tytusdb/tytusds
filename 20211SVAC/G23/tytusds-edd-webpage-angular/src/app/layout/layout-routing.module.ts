8a0b77232701cffb96c58a8cb4a6e5984ca261ec
OscarDOE <74039206+OscarDOE@users.noreply.github.com>
2021-06-15 13:20:38 -0600

    Se extrajo el zip
    Se extrajeron los archivos del zip, solo faltan los modules, pero se instalan por aparte

---
+++ 20211SVAC/G23/tytusds-edd-webpage-angular/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+            },
+            {
+                path: 'listas-edd/:idTipoLista',
+                loadChildren: () => import('./estructuras-lineales/listas-edd.module').then((m) => m.ListasEddModule)
+            }
+
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule {}
