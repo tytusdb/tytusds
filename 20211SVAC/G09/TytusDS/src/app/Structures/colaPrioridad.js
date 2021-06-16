6f1631cdded54bf3b1c08bc6432eede8f85820ca
Julissa <mjuligm1996@gmail.com>
2021-06-16 00:51:18 -0600

    Cola Prioridad Mejorada

--- 20211SVAC/G09/TytusDS/src/app/Structures/colaPrioridad.js
+++ 20211SVAC/G09/TytusDS/src/app/Structures/colaPrioridad.js
@@ -1,47 +1,100 @@
-function Cola(name,code){
-    this.name = name;
-         this.code = code; // El número entero indica la prioridad o gravedad de la condición del paciente
+class ColaPrioridad{
+	constructor(element, priority)
+	{
+		this.element = element;
+		this.priority = priority;
+	}
 }
-function Queue(){
-    this.data = [];
-         /*Entrada*/
-    this.enqueue = function(element){
-        this.data.push(element);
-    };
-         // Dequeue: Elimina el elemento con mayor prioridad en la cola. El elemento con el código de menor prioridad tiene la mayor prioridad. * /
-    this.dequeue = function(){
-        var priority = this.data[0].code;
-                 // Utilice el método de búsqueda secuencial para encontrar el elemento con la mayor prioridad, cuanto menor es el código de prioridad, mayor es la prioridad
-        for(var i=0; i<this.data.length; ++i){
-            if(this.data[i].code < priority){
-                priority = i;
+
+//controlador de la cola
+class PriorityQueue {
+	constructor()
+	{
+		this.items = [];
+	}
+
+// agrega un elemento y lo ordena por prioridad
+        enqueue(element, priority)
+        {
+            // crea el nuevo elemento
+            var ColaElemento= new ColaPrioridad(element, priority);
+            var contain = false;
+
+            // recorrer todo el arreglo
+            for (var i = 0; i < this.items.length; i++) {
+                //encontrar el espacio de prioridad
+                if (this.items[i].priority < ColaElemento.priority) {
+                    // al encotrar el espacio
+                    // agregar
+                    this.items.splice(i, 0, ColaElemento);
+                    contain = true;
+                    break;
+                }
+            }
+
+            // si no encontro su prioridad en el arreglo, agreguelo de ultimo
+            if (!contain) {
+                this.items.push(ColaElemento);
             }
         }
-        return this.data.splice(priority,1);
 
-    };
-    this.toString = function(){
-        var retstr = '';
-        for(var i=0; i<this.data.length; ++i){
-            retstr +=  this.data[i].name + " code: " + this.data[i].code + "\n";
+        //remover el dato de tipo FIFO
+        dequeue()
+        {
+        //mientras no este vacío
+            if (this.isEmpty())
+                return "borrado";
+            return this.items.shift();
         }
-        return retstr;
-    }
+
+        // retorna la mayor prioridad dada
+        front()
+        {
+           //mientras no este vacío
+            if (this.isEmpty())
+                return "No elements in Queue";
+            return this.items[0];
+        }
+
+        // retorna la menor prioridad dada
+        rear()
+        {
+             //mientras no este vacío
+            if (this.isEmpty())
+                return "No elements in Queue";
+            return this.items[this.items.length - 1];
+        }
+
+        // retorna si esta vacío o no
+        isEmpty()
+        {
+            // return true if the queue is empty.
+            return this.items.length == 0;
+        }
+
+        // retorna todo el contenido del arreglo
+        printPQueue()
+        {
+            var str = "";
+            for (var i = 0; i < this.items.length; i++)
+                str += this.items[i].element + " -> Prioridad: " + this.items[i].priority+ "\n" ;
+            return str;
+        }
 }
 
-/*test*/
-var queue = new Queue();
-var p = new Cola(1,5);
-queue.enqueue(p);
-p = new Cola(2,4);
-queue.enqueue(p);
-p = new Cola(3,6);
-queue.enqueue(p);
-p = new Cola(4,2);
-queue.enqueue(p);
-p = new Cola(5,1);
-queue.enqueue(p);
-console.log(queue.toString());
- 
-var seen = queue.dequeue();
-console.log(queue.toString());
+
+
+var priorityQueue = new PriorityQueue();
+
+priorityQueue.enqueue("5", 1);
+priorityQueue.enqueue("2", 5);
+priorityQueue.enqueue("3", 1);
+priorityQueue.enqueue("6", 2);
+priorityQueue.enqueue("4", 6);
+priorityQueue.enqueue("1", 2);
+
+console.log(priorityQueue.printPQueue());
+
+console.log("Eliminar elemento con mayor prioridad");
+priorityQueue.dequeue();
+console.log(priorityQueue.printPQueue());
