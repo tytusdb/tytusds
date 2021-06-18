30cf2189835c72a61c7d35cc65d85f41bed42772
Julissa <mjuligm1996@gmail.com>
2021-06-18 13:47:50 -0600

    Estructura Cola

--- 20211SVAC/G09/TytusDS/src/app/Structures/cola.js
+++ 20211SVAC/G09/TytusDS/src/app/Structures/cola.js
@@ -1,52 +1,59 @@
-class Cola{
-    constructor(){
-        this.cola = [];
+class NodoCola {
+    constructor(dato, siguiente) {
+      this.dato = dato;
+      this.siguiente = siguiente;
     }
-
-    //Verifica si la cola esta vacia
-    isEmpty(){
-        return this.cola.length === 0;
-    }
-
-    //Agrega un nuevo elemento de la cola
-    //Agrega al final de la cola
-    enqueue(dato){
-       this.cola.push(dato);
-       return this.cola;
-
-    }
-
-    //Elimina el primer elemento de la cola
-    dequeue(){
-        return this.cola.shift();
-    }
-
-    //Muestra el primer elemento de la cola
-    peek(){
-        return this.cola[0];
-    }
-
-    //Muestra el tamaño de la cola
-    size(){
-        return this.cola.length;
-    }
-
-    //Imprime los datos de la cola
-    print(){
-        return this.cola;
-    }
-
-
 }
+  
+class Cola {
+    constructor() {
+      this.primero = null;
+      this.guardarCola = null;
+    }
+
+    //Ingresar datos a la cola
+    enqueue(dato) {
+      const nuevo = new NodoCola(dato, null);
+      if (this.guardarCola) {
+        this.guardarCola.siguiente = nuevo;
+      }
+      this.guardarCola = nuevo
+      if (!this.primero) {
+        this.primero = this.guardarCola
+      }
+    }
+
+    //Sacar el primero de la cola
+    dequeue() {
+      if (!this.primero) {
+        return null;
+      }
+      const nodoPrimero = this.primero;
+      if (this.primero.siguiente) {
+        this.primero = this.primero.siguiente;
+      } else {
+        this.primero = null; 
+        this.guardarCola = null; 
+      }
+      return nodoPrimero;
+    }
+
+    //mostrar datos de la cola
+    print() {
+      let mostrarNodo = this.primero;
+      while (mostrarNodo) {
+        console.log(mostrarNodo.dato);
+        mostrarNodo = mostrarNodo.siguiente;
+      }
+    }
+  }
 
 const cola = new Cola();
-console.log("Tamaño de la cola: " + cola.size());//Tamaño 0
 console.log("Ingresando valores");
-console.log(cola.enqueue(1));
-console.log(cola.enqueue(2));
-console.log(cola.enqueue(3));
-console.log(cola.enqueue(4));
-console.log("Tamaño de la cola: " +cola.size());
-console.log("Primero de la cola: " +cola.peek()); //Muestra 1
-console.log("Sacamos primero de la cola: " +cola.dequeue()); //Sacamos 1
-console.log("Primero de la cola: " +cola.peek());//Muestra 2
+cola.enqueue("1");
+cola.enqueue("2");
+cola.enqueue("3");
+cola.print();
+console.log("Sacamos primero de la cola: ");
+cola.dequeue() //Sacamos 1
+cola.print();
