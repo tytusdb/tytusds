004066c3e3a3e0582e32facb89819d4c7aec790e
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-18 02:10:18 -0600

    printListaCirSimp

--- 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirS.js
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirS.js
@@ -1,9 +1,15 @@
-class Nodo{
+//Forma de insercion: Desordenada
+//Eliminacion Unica de primer dato encontrado
+//Busqueda de primer metodo encontrado
+
+class Nodo{ //Clase Nodo
+    //Constructor
     constructor(dato){
         this.siguiente = null;
         this.dato = dato;
     }
 
+    //Setters & Getters
     getDato(){
         return dato;
     }
@@ -13,20 +19,22 @@ class Nodo{
     }
 }
 
-class ListaCS{
+class ListaCS{ //Clase Lista Circular Simple
     constructor(){
         this.cabeza = null;
         this.cola = null;
         this.size = 0;
     }
 
+    //Metodo Insertar
     insert(dato){
         let nodo = new Nodo(dato);
+        //Insercion de primer nodo
         if(this.cabeza == null){
             this.cabeza = nodo;
             this.cola = nodo;
             this.size++;
-        }else if(this.cabeza != null){
+        }else if(this.cabeza != null){ // Insercion de nodo No Cabeza
             let aux = this.cabeza;
             while(aux != this.cola){
                 aux = aux.siguiente;
@@ -38,8 +46,15 @@ class ListaCS{
         }
     }
 
-    
-
-    
+    //Metodo Imprimir
+    print(){
+        let aux = this.cabeza;
+        while (aux != this.cola){
+            console.log(aux.dato);
+            aux = aux.siguiente
+        }
+        if(aux == this.cola){
+            console.log(aux.dato);
+        }
+    }
 }
-
