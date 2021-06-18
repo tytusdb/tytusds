7b003b5d873e48d1d6a613d615bdfd79b19dea7c
Ariel Macario <arielmacario.11@gmail.com>
2021-06-17 23:05:29 -0600

    Metodo Pop Cola

--- 20211SVAC/G03/src/estructuras/estructurasLineales/Cola.js
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/Cola.js
@@ -34,6 +34,23 @@ class Cola {
 
         this.longitud++
     }
+	
+	Pop(){
+        let eliminado = this.ultimo
+        if (this.primero == this.ultimo){
+            this.primero = null
+            this.ultimo = null
+        }else{
+            let nodoActual = this.primero
+            while (nodoActual.siguiente != eliminado){
+                nodoActual = nodoActual.siguiente
+            }
+
+            this.ultimo = nodoActual
+            this.ultimo.siguiente = null
+        }
+        this.longitud--
+    }
 
 }
 
