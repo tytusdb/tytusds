4e1a2a6d462ee4b6dfe3ece6fe8f98a03e622785
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-18 23:26:49 -0600

    BuscarListaCircDob

--- 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirD.js
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirD.js
@@ -1,10 +1,17 @@
-class Nodo{
+//Insertcion no ordenada
+//Busqueda unica
+//Eliminacion unica
+
+class Nodo{ //Clase Nodo
+    
+    //Constructor
     constructor(dato){
         this.siguiente = null;
         this.anterior = null;
         this.dato = dato;
     }
 
+    //Setters & Getters
     getDato(){
         return dato;
     }
@@ -14,20 +21,25 @@ class Nodo{
     }
 }
 
-class ListaCS{
+class ListaCD{  //Clase Lista Circular doble
+
+    //Constructor
     constructor(){
         this.cabeza = null;
         this.cola = null;
         this.size = 0;
     }
 
+    //Metodo Insertar
     insert(dato){
         let nodo = new Nodo(dato);
+        //Insertcion de primer nodo 
         if(this.cabeza == null){
             this.cabeza = nodo;
             this.cola = nodo;
             this.size++;
         }else if(this.cabeza != null){
+            //Insercion de Nodos no primeros
             let aux = this.cabeza;
             while(aux != this.cola){
                 aux = aux.siguiente;
@@ -41,6 +53,7 @@ class ListaCS{
         }
     }
 
+    //Metodo Imprimir
     print(){
         let aux = this.cabeza;
         console.log("Entro a print")
@@ -50,6 +63,24 @@ class ListaCS{
         }
         if(aux == this.cola){
             console.log(aux.dato);
+        }
+    }
+
+    //Metodo Buscar
+    buscar(dato){
+        let aux = this.cabeza;
+        let contador = 1;
+        while(aux.dato != dato && contador != this.size){
+            aux= aux.siguiente;
+            contador++;
+        }
+        if(aux.dato == dato){
+            //Dato encontrado
+            console.log(aux.dato + " fue encontrado exitosamente");
+        }
+        if (contador == this.size){
+            //Dato no encontrado
+            console.log("No se encontro el dato buscado");
         }
     }
 }
