6204ef4cc806153537e77137daf17b6242370bc1
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-18 22:52:29 -0600

    PrintListaCircDob

--- 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirD.js
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/ListaCirD.js
@@ -1,45 +1,55 @@
 class Nodo{
     constructor(dato){
-        this.siguiente = null
-        this.anterior = null
-        this.dato = dato
+        this.siguiente = null;
+        this.anterior = null;
+        this.dato = dato;
     }
 
     getDato(){
-        return dato
+        return dato;
     }
 
     setDato(dato){
-        this.dato = dato
+        this.dato = dato;
     }
 }
 
 class ListaCS{
     constructor(){
-        this.cabeza = null
-        this.cola = null
-        this.size = 0
+        this.cabeza = null;
+        this.cola = null;
+        this.size = 0;
     }
 
     insert(dato){
-        let nodo = new Nodo(dato)
+        let nodo = new Nodo(dato);
         if(this.cabeza == null){
-            this.cabeza = nodo
-            this.cola = nodo
-            this.size++
+            this.cabeza = nodo;
+            this.cola = nodo;
+            this.size++;
         }else if(this.cabeza != null){
             let aux = this.cabeza;
             while(aux != this.cola){
-                aux = aux.siguiente
+                aux = aux.siguiente;
             }
-            aux.siguiente = nodo
-            nodo.anterior = aux
-            nodo.siguiente = this.cabeza
-            this.cabeza.anterior = nodo
-            this.cola = nodo
-            this.size++
+            aux.siguiente = nodo;
+            nodo.anterior = aux;
+            nodo.siguiente = this.cabeza;
+            this.cabeza.anterior = nodo;
+            this.cola = nodo;
+            this.size++;
         }
     }
 
+    print(){
+        let aux = this.cabeza;
+        console.log("Entro a print")
+        while (aux != this.cola){
+            console.log(aux.dato);
+            aux = aux.siguiente
+        }
+        if(aux == this.cola){
+            console.log(aux.dato);
+        }
+    }
 }
-
