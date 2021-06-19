38067b79fb9f6858df61d31792cbee32e6064a46
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-19 17:33:13 -0600

    EliminarABB

--- 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ABB.js
+++ 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ABB.js
@@ -76,4 +76,277 @@ class ABB{
         }
         this.imprimir(this.raiz)
     }
+
+    //Metodo Eliminar
+    eliminando(dato){
+        //Caso1: no existe arbol
+        if(this.raiz ==null){
+            console.log("No existe arbol")
+            return
+        }
+        //asignando valor inicial
+        let nodo = this.raiz
+        this.eliminar(dato, nodo)
+    }
+
+    //Sub Metodo Eliminar
+    eliminar(dato, nodo){
+        //Caso2: el dato a buscar se encuentra en la raiz
+        if(dato == this.raiz.dato){
+            if(nodo.izquierda != null){
+                let nodiz = nodo.izquierda
+                let maxiz= null
+                if(nodiz.derecha != null){
+                    maxiz = this.maxIzq(nodo.izquierda)
+                }else{
+                    maxiz = nodo.izquierda
+                    if(nodo.derecha != null){
+                        let enlace = nodo.derecha
+                        maxiz.derecha = enlace
+                        nodo.derecha = null
+                    }
+                    nodo.izquierda = null
+                    console.log("se elimino el nodo")
+                    return
+                }
+                let nodmax = new nodo(maxiz)
+                let trasiz = null
+                let trasder = null
+                if(nodo.izquierda!=null){
+                    trasiz = nodo.izquierda
+                }
+                if(nodo.derecha != null){
+                    trasder = nodo.derecha
+                }
+                if(trasiz != null){
+                    nodmax.izquierda = trasiz
+                    nodo.izquierda = null
+                }
+                if(trasder != null){
+                    nodmax.derecha = trasder
+                    nodo.derecha = null
+                }
+                console.log("se elimino el nodo")
+                return
+            }else if(nodo.izquierda == null && nodo.derecha != null){
+                let nodiz = nodo.derecha
+                let minder= null
+                if(nodiz.izquierda != null){
+                    minder = this.minDer(nodo.derecha) // minder con valor de dato
+                }else{
+                    minder = nodo.derecha // miner con valor de nodo
+                    if(nodo.izquierda != null){
+                        let enlace = nodo.izquierda
+                        minder.izquierda = enlace
+                        nodo.izquierda = null
+                    }
+                    this.raiz = nodo.derecha
+                    nodo.derecha = null
+                    console.log("se elimino el nodo")
+                    return
+                }
+                let nodmin = new nodo(minder)
+                let trasiz = null
+                let trasder = null
+                if(nodo.izquierda!=null){
+                    trasiz = nodo.izquierda
+                }
+                if(nodo.derecha != null){
+                    trasder = nodo.derecha
+                }
+                if(trasiz != null){
+                    nodmin.izquierda = trasiz
+                    nodo.izquierda = null
+                }
+                if(trasder != null){
+                    nodmin.derecha = trasder
+                    nodo.derecha = null
+                }
+                this.raiz = nodmin
+                console.log("se elimino el nodo")
+                return
+            }else{
+                this.raiz = null
+                return
+            }
+        }
+        //Demas Casos: eliminacion dentro del arbol
+        if(nodo.izquierda!= null){
+            //Dato encontrado a la izquierda
+            if(dato == nodo.izquierda.dato){
+                let tmp = nodo.izquierda
+                //Caso3: Eliminacion de hijo a la izquierda
+                if(tmp.izquierda == null && tmp.derecha == null){
+                    nodo.izquierda = null
+                    console.log("nodo hoja fue eliminado con exito")
+                    return
+                }else{ // Caso 4: Eliminacion de no hijo a la izquierda
+                    if(tmp.izquierda != null){ // Reestructuracion de arbol y eliminacion de nodo
+                        let nodiz = tmp.izquierda
+                        let maxiz= null
+                        if(nodiz.derecha != null){
+                            maxiz = this.maxIzq(tmp.izquierda) // maxiz con valor de dato 
+                        }else{
+                            maxiz = tmp.izquierda // maxiz con valor de nodo
+                            if(tmp.derecha != null){
+                                let enlace = tmp.derecha
+                                maxiz.derecha = enlace
+                                tmp.derecha = null
+                            }
+                            tmp.izquierda = null
+                            nodo.izquierda = maxiz
+                            console.log("se elimino el nodo")
+                            return
+                        }
+                        let nodmax = new nodo(maxiz)
+                        let trasiz = null
+                        let trasder = null
+                        if(tmp.izquierda!=null){
+                            trasiz = tmp.izquierda
+                        }
+                        if(tmp.derecha != null){
+                            trasder = tmp.derecha
+                        }
+                        if(trasiz != null){
+                            nodmax.izquierda = trasiz
+                            tmp.izquierda = null
+                        }
+                        if(trasder != null){
+                            nodmax.derecha = trasder
+                            tmp.derecha = null
+                        }
+                        nodo.izquierda = nodmax
+                        console.log("se elimino el nodo")
+                        return
+                    }else if(tmp.izquierda == nul && tmp.derecha != null){ // Reestructuracion de arbol y eliminacion de nodo
+                        let nodiz = tmp.derecha
+                        let minder= null
+                        if(nodiz.izquierda != null){
+                            minder = this.minDer(tmp.derecha) // minder con valor de dato 
+                        }else{
+                            minder = tmp.derecha // minder con valor de nodo
+                            if(tmp.izquierda != null){
+                                let enlace = tmp.izquierda
+                                minder.izquierda = enlace
+                                tmp.izquierda = null
+                            }
+                            nodo.derecha = minder
+                            tmp.derecha = null
+                            console.log("se elimino el nodo")
+                            return
+                        }
+                        let nodmin = new nodo(minder)
+                        let trasiz = null
+                        let trasder = null
+                        if(tmp.izquierda!=null){
+                            trasiz = tmp.izquierda
+                        }
+                        if(tmp.derecha != null){
+                            trasder = tmp.derecha
+                        }
+                        if(trasiz != null){
+                            nodmin.izquierda = trasiz
+                            tmp.izquierda = null
+                        }
+                        if(trasder != null){
+                            nodmin.derecha = trasder
+                            tmp.derecha = null
+                        }
+                        nodo.derecha = nodmin
+                        console.log("se elimino el nodo")
+                        return
+                    }
+                }
+            }
+        }
+        if(nodo.derecha != null){
+ // Caso 5: Eliminacion de hijo a la derecha
+            if(dato == nodo.derecha.dato){
+                let tmp = nodo.derecha
+                if(tmp.izquierda == null && tmp.derecha == null){
+                    nodo.derecha = null
+                    console.log("nodo hoja fue eliminado con exito")
+                    return
+                }else{ // Caso 6: Eliminacion de no hijo a la derecha
+                    if(tmp.derecha != null){ // Reestructuracion de arbol y eliminacion de nodo
+                        let nodiz = tmp.derecha
+                        let minder= null
+                        if(nodiz.izquierda != null){
+                            minder = this.minDer(tmp.derecha) // minder con valor de dato 
+                        }else{
+                            minder = tmp.derecha // minder con valor de nodo
+                            if(tmp.izquierda != null){
+                                let enlace = tmp.izquierda
+                                minder.izquierda = enlace
+                                tmp.izquierda = null
+                            }
+                            nodo.derecha = minder
+                            tmp.derecha = null
+                            console.log("se elimino el nodo")
+                            return
+                        }
+                        let nodmin = new nodo(minder)
+                        let trasiz = null
+                        let trasder = null
+                        if(tmp.izquierda!=null){
+                            trasiz = tmp.izquierda
+                        }
+                        if(tmp.derecha != null){
+                            trasder = tmp.derecha
+                        }
+                        if(trasiz != null){
+                            nodmin.izquierda = trasiz
+                            tmp.izquierda = null
+                        }
+                        if(trasder != null){
+                            nodmin.derecha = trasder
+                            tmp.derecha = null
+                        }
+                        nodo.derecha = nodmin
+                    }else if(tmp.derecha == null && tmp.izquierda != null){ // Reestructuracion de arbol y eliminacion de nodo
+                        let nodiz = tmp.izquierda
+                        let maxiz= null
+                        if(nodiz.derecha != null){
+                            maxiz = this.maxIzq(tmp.izquierda) // maxiz con valor de dato 
+                        }else{
+                            maxiz = tmp.izquierda // maxiz con valor de nodo
+                            if(tmp.derecha != null){
+                                let enlace = tmp.derecha
+                                maxiz.derecha = enlace
+                                tmp.derecha = null
+                            }
+                            tmp.izquierda = null
+                            nodo.izquierda = maxiz
+                            console.log("se elimino el nodo")
+                            return
+                        }
+                        let nodmax = new nodo(maxiz)
+                        let trasiz = null
+                        let trasder = null
+                        if(tmp.izquierda!=null){
+                            trasiz = tmp.izquierda
+                        }
+                        if(tmp.derecha != null){
+                            trasder = tmp.derecha
+                        }
+                        if(trasiz != null){
+                            nodmax.izquierda = trasiz
+                            tmp.izquierda = null
+                        }
+                        if(trasder != null){
+                            nodmax.derecha = trasder
+                            tmp.derecha = null
+                        }
+                        nodo.izquierda = nodmax
+                    }
+                }
+            }
+        }
+        if(nodo.izquierda !=null){
+            this.eliminar(dato, nodo.izquierda)
+        }
+       if(nodo.derecha != null){
+            this.eliminar(dato, nodo.derecha)
+       }
+    }
 }
