a65155165bba998aeec902849f578213d72b7583
Jecm97 <3206447822001@ingenieria.usac.edu.gt>
2021-06-20 15:08:40 -0600

    Revert "Revert "Merge branch 'main' of https://github.com/tytusdb/tytusds into main""
    This reverts commit f39c7f9a520db3a60dc9f95c47375d70b2d682d2.

--- 20211SVAC/G05/TytusDS/src/app/PaginasWeb/listas-circulares/js/main.js
+++ 20211SVAC/G05/TytusDS/src/app/PaginasWeb/listas-circulares/js/main.js
@@ -2,7 +2,9 @@ let Listacircular=require('./ListaCircular');
 let l=new Listacircular();
 l.appendO(2);
 l.appendO(4);
-l.imprimir();
-l.eliminar(2);
+l.appendO(8);
+l.appendO(9);
+
+console.log(l.indexBusqueda("2"))
 l.imprimir();
 
