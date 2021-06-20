1c7d104c040f2bab84e50d80c62fdc04942a1c8f
Erick Mayorga <emayorga30@gmail.com>
2021-06-19 22:46:12 -0600

    Inicio Lista Simple

---
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/ListaSimple.js
@@ -0,0 +1,20 @@
+class Nodo {
+    constructor(valor){
+        this.valor = valor;
+        this.siguiente = null;
+        this.anterior =  null;
+    }
+   
+}
+class ListaSimple{
+   
+
+    constructor(){
+       this.primero = null;               
+       this.contador = 0; 
+       var contadorListas;
+       const fs = require("fs");       
+    }    
+  
+}
+module.export = ListaSimple;
