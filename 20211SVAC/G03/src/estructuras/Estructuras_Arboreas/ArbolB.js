d8928c4c727414d56348f18bace85fc1d887f65d
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-20 00:18:55 -0600

    PrintArbolB

--- 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ArbolB.js
+++ 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ArbolB.js
@@ -1,4 +1,6 @@
+//Clase Nodo
 class Nodo{
+    //Constructor
     constructor(dato){
         this.dato = dato
         this.anterior = null
@@ -8,24 +10,30 @@ class Nodo{
     }
 }
 
+//Arreglo de uso unico
 var arr = []
 
+//Clase Rama
 class Rama{
+    //Constructor
     constructor(){
         this.contador = 0
         this.hoja = true
         this.raiz = null
     }
 
+    //Metodo Insertar en Rama
     insertar(nodo){
+        //Insertar Primer Nodo
         if(this.raiz == null){
             this.raiz = nodo
             this.contador++
-        }else{
+        }else{  //Insertar Nodo no Primero
             let tmp = this.raiz
             do{
-                if(nodo.dato<=tmp.dato){
+                if(nodo.dato<=tmp.dato){    //Insercion de dato menor igual
                     this.contador++
+                    //Insercion en nodo no vacio
                     if(tmp == this.raiz){
                         this.raiz.anterior = nodo
                         nodo.siguiente = this.raiz
@@ -33,6 +41,7 @@ class Rama{
                         this.raiz = nodo
                         break
                     }else{
+                        //Insercion entre nodos
                         nodo.anterior = tmp.anterior
                         nodo.siguiente = tmp
                         tmp.anterior.siguiente = nodo
@@ -41,6 +50,7 @@ class Rama{
                         tmp.izquierdo = nodo.derecho
                         break
                     }
+                //Insercion en nodos vacios
                 }else if(tmp.siguiente == null){
                     this.contador++
                     tmp.siguiente = nodo
@@ -56,19 +66,24 @@ class Rama{
 
 }
 
+//Clase Arbol
 class ArbolB{
+    //Constructor
     constructor(orden){
         this.raiz = null
         this.orden = orden
     }
 
+    //Metodo Insercion en arbol
     insertar(dato){
         let nodo = new Nodo(dato)
+        //Insercion para arbol vacio
         if(this.raiz == null){
             this.raiz = new Rama()
             this.raiz.insertar(nodo)
             return
         }else{
+            //Insercion para arbol no vacio
             let tmp = this.add(nodo, this.raiz)
             if(tmp instanceof Nodo){
                 this.raiz = new Rama()
@@ -78,20 +93,26 @@ class ArbolB{
         }
     }
 
+    //Metodo para agregar y verificar estado de las ramas
     add(nodo, rama){
+        //Si es un nodo hijo
         if(rama.hoja){
             rama.insertar(nodo)
             if(rama.contador == this.orden){
+                //Division de rama
                 return this.divRam(rama)
             }else{
+                //Retornar rama no llena
                 return rama
             }
+        //Si no es un nodo hijo
         }else{
             let tmp = rama.raiz 
             do{
                 if(nodo.dato == tmp.dato){
+                    //No inserta duplicados
                     return rama
-                }else if(nodo.dato < tmp.dato){
+                }else if(nodo.dato < tmp.dato){ //Insercion de nodo menor
                     let aux = this.add(nodo, tmp.izquierdo)
                     if( aux instanceof Nodo){
                         rama.insertar(aux)
@@ -100,7 +121,7 @@ class ArbolB{
                         }
                     }
                     return rama
-                }else if(tmp.siguiente == null){
+                }else if(tmp.siguiente == null){    //Insercion para nodo con un solo valor
                     let aux = this.add(nodo, tmp.derecho)
                     if(aux instanceof Nodo){
                         rama.insertar(aux)
@@ -116,6 +137,7 @@ class ArbolB{
         return rama
     }
 
+    //Metodo de division de rama
     divRam(rama){
         let der = new Rama()
         let izq = new Rama()
@@ -144,9 +166,40 @@ class ArbolB{
         mitad.derecho = der
         return mitad
     }
-    
+
+    //Metodo de impresion de Arbol
+    printArbol(){
+        if (this.raiz == null){
+            console.log("no existe arbol")
+            return 
+        }
+        this.subprint(this.raiz) 
+    }
+
+    //Sub metodo de impresion de arbol
+    subprint(rama){
+        if (rama.raiz == null){
+            console.log("no hay nodos")
+            return 
+        }
+        let aux = rama.raiz
+        if(aux.izquierdo != null){
+            this.subprint(aux.izquierdo)
+        }
+        if(aux.derecho!=null){
+            this.subprint(aux.derecho)
+        }
+        if(aux.anterior == null && aux.siguiente == null){
+            console.log(aux.dato)
+            return
+        }
+        while(aux!= null){
+            console.log(aux.dato)
+            aux = aux.siguiente
+            if(aux != null && aux.derecho!=null){
+                this.subprint(aux.derecho)
+            }
+        }
+    }
+
 }
-
-
-
-
