dd05de335a26a2a5d0bc3bf5e4990c34befc0b1b
Erick Mayorga <emayorga30@gmail.com>
2021-06-20 00:42:36 -0600

    Inicio Bloque Agregar

--- 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ArbolBplus.js
+++ 20211SVAC/G03/src/estructuras/Estructuras_Arboreas/ArbolBplus.js
@@ -17,7 +17,44 @@ class Rama {
         this.ramaPadre = null;
     }
 
-    
+    agregarNodo(nodo) {
+        if (this.indice == null) {
+            this.indice = nodo;
+            this.contador++;
+        } else {
+            let temporal = this.indice;
+            do {
+                if (nodo.valor <= temporal.valor) {
+                    this.contador++;
+                    if (temporal == this.indice) {
+                        temporal.anterior = nodo;
+                        nodo.siguiente = temporal;
+                        temporal.rama_Izq = nodo.rama_Der;
+                        this.indice = nodo;
+                        break;
+                    } else {//en caso de que no sea el indice se inserta en medio de los valores respectivos
+                        nodo.anterior = temporal.anterior;
+                        nodo.siguiente = temporal;
+                        temporal.anterior.siguiente = nodo;
+                        temporal.anterior = nodo;
+
+                        temporal.anterior.rama_Der = nodo.rama_Izq;
+                        temporal.rama_Izq = nodo.rama_Der;
+                        break;
+
+                    }
+                }// insertar al final
+                else if (temporal.siguiente == null) {
+                    this.contador++;
+                    temporal.siguiente = nodo;
+                    temporal.rama_Der = nodo.rama_Izq;
+                    nodo.anterior = temporal;
+                    break;
+                }
+                temporal = temporal.siguiente;
+            } while (temporal != null);
+        }
+    }
 }
 
 class ArbolBplus {
@@ -26,6 +63,31 @@ class ArbolBplus {
         this.NodoAux = null;
         this.raiz = null;
 
+    }
+
+/*-----------------------------------------------
+-------------Inicio bloque Agregar --------------
+-----------------------------------------------*/
+
+    agregar(valor) {
+        let nodo = new Nodo(valor);
+
+        this._agregar(nodo, this.raiz)
+
+    }
+    _agregar(nodo, ramaAux) {
+        if (this.raiz == null) {
+            this.raiz = new Rama();
+            this.raiz.agregarNodo(nodo);
+
+        } else if (ramaAux.hoja) {
+            ramaAux.agregarNodo(nodo);
+            if (ramaAux.contador == this.orden) {
+                this.dividirRama(ramaAux);
+            }
+        } else if (ramaAux.hoja == false) {
+            this.buscarInsercion(nodo, ramaAux);
+        }
     }
     
 
