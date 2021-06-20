f9e0040bf0f8ca95db23510f0da7376dd061be8e
Erick Mayorga <emayorga30@gmail.com>
2021-06-20 01:16:22 -0600

    AgragarLista Simple

--- 20211SVAC/G03/src/estructuras/estructurasLineales/ListaSimple.js
+++ 20211SVAC/G03/src/estructuras/estructurasLineales/ListaSimple.js
@@ -14,7 +14,30 @@ class ListaSimple{
        this.contador = 0; 
        var contadorListas;
        const fs = require("fs");       
-    }    
+    }   
+
+
+    agregar(elemento) {
+        //crear un nodo para agregar a la lista
+        let nodo = new Nodo(elemento)
+        this.contador++;
+        if (this.primero == null) {
+            this.primero = nodo;
+
+        }
+        else {
+            // Busca el final del arreglo e ingresa el nodo ahi
+            let temporal = this.primero;
+            while (temportal.siguiente != null) {
+                temporal = temporal.siguiente;
+
+            }
+            nodo.id = contador;
+            temporal.siguiente = nodo;
+        }
+
+    }
+ 
   
 }
 module.export = ListaSimple;
