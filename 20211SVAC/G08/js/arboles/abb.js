6520f904c6c1d73d3ac1fd97bcf5cc09fb21f877
Victor Cuches de León <69135983+VictorCuches@users.noreply.github.com>
2021-06-20 04:04:22 -0600

    Agregando eventos al BSTree

--- 20211SVAC/G08/js/arboles/abb.js
+++ 20211SVAC/G08/js/arboles/abb.js
@@ -1,3 +1,7 @@
+// C  O  D  I  G  O
+// C  L  A  S  E  S
+// Programando el BSTree
+
 class Nodo {
     constructor(dato){
         this.dato = dato;
@@ -11,7 +15,7 @@ class BSTree{
     constructor(){
         this.root = null;
     }
-    insertar(dato){
+    agregar(dato){
         let nodo = new Nodo(dato);
         if(this.root == null){
             this.root = nodo;
@@ -79,6 +83,7 @@ class BSTree{
 }
 
 const arbol = new BSTree();
+/*
 arbol.insertar(15);
 arbol.insertar(25);
 arbol.insertar(10);
@@ -98,3 +103,134 @@ console.log("-------------");
 arbol.postOrder(root);
 console.log("-------------");
 console.log(arbol.buscar(root, 25));
+*/
+
+// E  V  E  N  T  O  S
+// ----- AGREGAR -----
+const btn_Agregar = document.getElementById('agregar');
+btn_Agregar.addEventListener('click', agregar);
+
+// ----- LIMPIAR -----
+const btn_Limpiar = document.getElementById('limpiar');
+btn_Limpiar.addEventListener('click', limpiar);
+
+// ----- ESPACIO -----
+var container = document.getElementById("espacioDraw");
+
+// ----- REPETIR -----
+const btn_Repetir = document.getElementById('repeticion');
+//btn_Repetir.addEventListener('click', repetir);
+
+// ----- ELIMINAR -----
+const btn_Eliminar = document.getElementById('eliminar');
+btn_Eliminar.addEventListener('click', eliminar);
+
+// ----- BUSCAR -----
+const btn_Buscar= document.getElementById('buscar');
+btn_Buscar.addEventListener('click', buscar);
+
+// ----- VELOCIDAD -----
+const btn_Velocidad = document.getElementById('velocidad');
+btn_Velocidad.addEventListener('click', getVelocidad);
+
+// ----- ACTUALIZAR -----
+const btn_Actualizar = document.getElementById('actualizar');
+btn_Actualizar.addEventListener('click', actualizar);
+
+// ----- GUARDAR JSON -----
+const btn_Guardar = document.getElementById('guardar');
+btn_Guardar.addEventListener('click', guardar);
+
+// V  A  R  I  A  B  L  E  S  -  G  L  O  B  A  L  E  S
+var velocidad;
+var tipoDato;
+
+
+// F  U  N  C  I  O  N  E  S  -  E  V  E  N  T  O  S
+function agregar(){
+    // Elemento ingresado en el textbox
+    var elemento = document.getElementById('elemento');
+    // Verificacion de repeticion de valores
+    var repe = cola.verificar(spc_elemento.value);
+    var chek_Repe = btn_Repetir.checked;
+    if (repe == true && chek_Repe == true){
+        
+        alert("No se pueden repetir valores");
+        spc_elemento.value ="";
+        spc_elemento.focus();
+        
+    } else {
+        agregar2(spc_elemento,spc_elemento.value);
+    }
+}
+
+function agregar2(box, dato){
+    // Insertando elemento en el arbol
+    arbol.agregar(elemento);
+    // Creacion del arbol
+
+    box.value = "";
+    box.focus();
+}
+
+// ***** ELIMINAR ELEMENTO *****
+function eliminar(){
+    console.log("Eliminando");
+}
+
+// ***** BUSCAR ELEMENTO ***** 
+function buscar(){
+    console.log("Buscando");
+}
+
+// ***** ACTUALIZAR ELEMENTO ***** 
+function actualizar(){
+    console.log("Actualizando");
+}
+
+// ***** GUARDAR JSON *****
+function guardar(){
+    console.log("Guardando");
+}
+
+
+// ***** LEER ARCHIVO JSON *****
+function readFile(evento){ 
+    
+    let archivo = evento.target.files[0];
+    if (archivo){
+        let reader = new FileReader();
+        reader.onload = function(e){
+            contenido = e.target.result;
+            // console.log(contenido)
+            console.log("-----------")
+            convert = JSON.parse(contenido);
+            console.log("VALORES")
+            listaValores = convert.valores;
+            tipoDato = typeof(listaValores[0]);
+            agregarFile();
+
+                      
+            //generateElements(listaValores, tipoDato);
+
+        };
+        reader.readAsText(archivo); 
+
+    } else {
+        alert("No se ha seleccionado ningun archivo");
+    }
+}
+window.addEventListener('load', ()=>{ // cada vez que cambie 
+    document.getElementById('file').addEventListener('change',readFile)
+});
+
+// ***** LIMPIAR *****
+function limpiar(){
+    window.location.reload();
+}
+
+// ***** OBTENER VELOCIDAD *****
+function getVelocidad(){
+    velocidad = document.getElementById("numVelocidad").value;
+    
+}
