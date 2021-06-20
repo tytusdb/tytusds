532e5c661e5c2ca32c22a822dc22c1a9bf22e131
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-20 02:36:16 -0600

    CoreccionOrdenamientoRapido

--- 20211SVAC/G03/src/estructuras/ordenamientos/OrdRapido.js
+++ 20211SVAC/G03/src/estructuras/ordenamientos/OrdRapido.js
@@ -1,22 +1,17 @@
-// Ordenamiento Rapido
-export default function ordRapido(arreglo){
+//Funcion de Ordenamiento rapido
+function ordRapido(arreglo){
+    //Posible Arreglo de un solo dato
     if (arreglo.length ===1){
         return arreglo;
     }
     const pivote = arreglo[arreglo.length - 1]
     const partIzq = []
     const partDer = []
+    //Iterador
     for (const el of arreglo.slice(0, arreglo.length - 1)){
         el<pivote ? partIzq.push(el) : partDer.push(el)
     }
-    for ( let i = 0; i<arreglo.length-1; i++){
-        if(arreglo[i]<pivote){
-            partIzq.push(arreglo[i])
-        } else{
-            partDer.push(arreglo[i])
-        }
-    }    
-    //Llenado de arreglo
+    //Construccion de salida
     if(partIzq.length>0 && partDer > 0){
         return [...ordRapido(partIzq),pivote,...ordRapido(partDer)]
     } else if(partIzq.length> 0){
