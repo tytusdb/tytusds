5a604fe91e3bbccfa8e73d80dec98505a8b45ea8
laraajosee <chemanuel94@gmail.com>
2021-06-21 01:25:21 -0600

    .

--- 20211SVAC/G09/TytusDS/src/app/Ordenamientos/Ordenamientos.js
+++ 20211SVAC/G09/TytusDS/src/app/Ordenamientos/Ordenamientos.js
@@ -127,8 +127,50 @@ function compararString(var1, var2){
     }
     return popo
 }
-lista = ["juli","daniel","miguel","che",'la',"ferggie","o"];
-NombresAscci = ObtenerString(lista);
-NombresAscciOrdenados = ordBurburja(NombresAscci);
-accion = compararString(NombresAscciOrdenados,lista);
-console.log(accion)
+//lista = ["juli","daniel","miguel","che",'la',"ferggie","o"];
+//NombresAscci = ObtenerString(lista);
+//NombresAscciOrdenados = ordBurburja(NombresAscci);
+//accion = compararString(NombresAscciOrdenados,lista);
+//console.log(accion)
+
+function AbrirOrdenamientoBurbuja(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    
+    reader.onload = function(event) {
+      // El texto del archivo se mostrará por consola aquí
+     // console.log(event.target.result)
+      let doc = JSON.parse(event.target.result);
+      
+      //console.log(doc)
+
+      for (var key in doc) {
+        //console.log('name=' + key + ' value=' + doc[key]);
+        if(key=='categoria'){
+            categoriaPrio = doc[key]
+            console.log(categoria)
+        }
+        if(key=='nombre'){
+            nombrePrio = doc[key]
+            console.log(nombre)
+        }
+        if(key=='repeticion'){
+            repeticionPrio = doc[key]
+            console.log(repeticion)
+        }
+        if(key=='animacion'){
+            animacionPrio = doc[key]
+            console.log(animacion)
+        }
+        if(key=='valores'){
+            //console.log(doc[key].length)
+            for (var k in doc[key]){
+                ListaAOrdenar.push(doc[key][k]);
+                console.log(doc[key][k])
+            } 
+        }
+     }
+     
+    };
+    
+}//guardar archivo
--- 20211SVAC/G09/TytusDS/src/app/Structures/colaPrioridad.js
+++ 20211SVAC/G09/TytusDS/src/app/Structures/colaPrioridad.js
@@ -85,6 +85,11 @@ class PriorityQueue {
 
 
 var priorityQueue = new PriorityQueue();
+var categoriaPrio = "Estructura Lineal";
+var nombrePrio = 'Cola Prioridad';
+var repeticionPrio = "True";
+var animacionPrio = "0";
+
 function insertarPrioridad(var1,var2){
     priorityQueue.enqueue(var1,var2)
     console.log(priorityQueue.printPQueue());
@@ -102,3 +107,61 @@ function insertarPrioridad(var1,var2){
 //console.log("Eliminar elemento con mayor prioridad");
 //priorityQueue.dequeue();
 //console.log(priorityQueue.printPQueue());
+function AbrirPrioridad(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      // El texto del archivo se mostrará por consola aquí
+     // console.log(event.target.result)
+      let doc = JSON.parse(event.target.result);
+      //console.log(doc)
+
+      for (var key in doc) {
+        //console.log('name=' + key + ' value=' + doc[key]);
+        if(key=='categoria'){
+            categoriaPrio = doc[key]
+            console.log(categoria)
+        }
+        if(key=='nombre'){
+            nombrePrio = doc[key]
+            console.log(nombre)
+        }
+        if(key=='repeticion'){
+            repeticionPrio = doc[key]
+            console.log(repeticion)
+        }
+        if(key=='animacion'){
+            animacionPrio = doc[key]
+            console.log(animacion)
+        }
+        if(key=='valores'){
+            //console.log(doc[key].length)
+            for (var k in doc[key]){
+                priorityQueue.enqueue(doc[key][k]['valor'],doc[key][k]['prioridad'])
+            }
+        }
+     }console.log(priorityQueue.printPQueue())
+     
+
+    };
+
+    reader.readAsText(file);
+}//guardar archivo
+function downloadPrioridad(filename, text) {
+    
+    lista = listSimple.print()
+  
+    var element = document.createElement('a');
+    let doc = JSON.stringify({ "categoria": categoriaPrio, 'nombre': nombrePrio, 'repeticion':repeticionPrio, 'animacion':animacionPrio, 'valores': lista });
+    
+    //console.log(listSimple.print())
+    element.setAttribute('href', 'data:json,' + doc);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
