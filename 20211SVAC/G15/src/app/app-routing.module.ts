aab86ae69c5fc91c9983dc605698bfd6709a6820
Grethel Minerva Vilchez Suarez <3031381490108@ingenieria.usac.edu.gt>
2021-06-20 20:32:05 -0600

    DisenioHome

--- 20211SVAC/G15/src/app/app-routing.module.ts
+++ 20211SVAC/G15/src/app/app-routing.module.ts
@@ -5,10 +5,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component"
 import { ListaDobleCircularComponent} from "./components/listadobleCircular/listadobleCircular.component"
 import { ListaCircularSimpleComponent } from "./components/lista-circular-simple/lista-circular-simple.component"
+import { ListaDobleComponent } from "./components/lista-doble/lista-doble.component"
+import { ColaComponent } from "./components/cola/cola.component"
+import { ColaPrioridadComponent } from './components/cola-prioridad/cola-prioridad.component';
 
 const routes: Routes = [
   {path: 'listaDobleCircular', component: ListaDobleCircularComponent, pathMatch: 'full'},
   {path: 'listaSimpleCircular', component: ListaCircularSimpleComponent, pathMatch: 'full'},
+  {path: 'listadoblementeEnlazada', component: ListaDobleComponent, pathMatch: 'full'},
+  {path: 'cola', component: ColaComponent, pathMatch: 'full'},
+  {path: 'colaPrioridad' , component:ColaPrioridadComponent, pathMatch: 'full'},
+  
   {path: '', component: HomeComponent, pathMatch: 'full'}
   
 ];
