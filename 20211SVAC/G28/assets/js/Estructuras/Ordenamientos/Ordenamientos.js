9530b59bda8f6c5d48cdd29104a5a26fb43e1f3f
SteveGonzalez201900122 <74296161+SteveGonzalez201900122@users.noreply.github.com>
2021-06-22 13:05:04 -0600

    orden
    coreccion de nombre de archivo de ordenamientos

--- 20211SVAC/G28/assets/js/Estructuras/Ordenamientos/Ordenamientos.js
+++ 20211SVAC/G28/assets/js/Estructuras/Ordenamientos/Ordenamientos.js
@@ -1,72 +1,89 @@
 function Burbuja(lista){
-        numeros = lista;
-        for (i = 0; i<numeros.length-1; i++){
-            for (j = i+1; j<numeros.length; j++){
-                if(numeros[i]>numeros[j]){
-                auxiliar = numeros[i];
-                numeros[i] = numeros[j];
-                numeros[j] = auxiliar;
-                }
+    numeros = lista;
+    for (i = 0; i<numeros.length-1; i++){
+        console.log("ITERACION: "+i);
+        console.log(numeros);
+        for (j = i+1; j<numeros.length; j++){
+            if(numeros[i]>numeros[j]){
+            const auxiliar = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = auxiliar;
+            console.log(numeros);
             }
         }
-        return numeros;
     }
+    return numeros;
+}
 
 function Seleccion(lista){
-        numeros = lista;
-        for (i = 0; i<numeros.length-1; i++){
-            menor = 0;
-            for (j = i+1; j<numeros.length; j++){
-                if(numeros[menor]>numeros[j]){
-                    menor = j;
-                }
+    var numeros = lista;
+    for (let i = 0; i<numeros.length-1; i++){
+        console.log("ITERACION: "+i);
+        console.log(numeros);
+        var menor = i;
+        for (let j = i+1; j<numeros.length; j++){
+            if(numeros[menor]>numeros[j]){
+                menor = j;
             }
-            temporal = numeros[i];
-            numeros[i] = numeros[menor];
-            numeros[menor] = temporal;
         }
-        return numeros;
+        const temporal = numeros[i];
+        numeros[i] = numeros[menor];
+        numeros[menor] = temporal;
+        console.log(numeros);
     }
+    return numeros;
+}
 
 function Insercion(lista){
-        numeros = lista;
-        for(i=0; i<numeros.length; i++){
-            auxiliar = numeros[i];
-            j = i;
-            while(j>0 && numeros[j-1]>auxiliar){
-                numeros[j] = numeros[j-1];
-                j--;
-            }
-            numeros[j] = auxiliar;
+    numeros = lista;
+    for(i=0; i<numeros.length; i++){
+        console.log("ITERACION: "+i);
+        console.log(numeros);
+        auxiliar = numeros[i];
+        j = i;
+        while(j>0 && numeros[j-1]>auxiliar){
+            numeros[j] = numeros[j-1];
+            j--;
         }
-        return numeros;
+        numeros[j] = auxiliar;
+        console.log(numeros);
     }
+    return numeros;
+}
 
-function Quicksort(a){
-        primero = 0;
-        ultimo = a.length-1;
-        central = (primero+ultimo)/2;
-        pivote = a[central];
-        i = primero, j = ultimo;
-        do{
-            while(a[i]<pivote){ i++}
-            while(a[j]>pivote){ i++}
-            if(i<=j){
-                aux = a[i];
-                a[i] = a[j];
-                a[j] = auxiliar;
-                i++;
-                j++;
-            }
-        }while (i<=j);
-        if(primero<j){
-            this,Quicksort(a, primero, j);
+function QuickSort(n){
+    var izquierda = 0;
+    var derecha = n.length-1;
+    var pivote = n[izquierda];
+    var i = izquierda;
+    var d = derecha;
+    while (i < d) {
+        while (n[i] <= pivote && i < d) {
+            i++;
         }
-        if(i<ultimo){
-            this.Quicksort(a, i, ultimo);
+        while (n[d] > pivote) {
+            d--;
         }
-        return a;
+        if (i < d) {
+            var aux = n[i];
+            n[i] = n[d];
+            n[d] = aux;
+        }
     }
-    
+    n[izquierda] = n[d];
+        n[d] = pivote;
 
-module.exports = Burbuja;
+        if (izquierda < d - 1) {
+            QuickSort(n, izquierda, d - 1);
+        }
+        if (d + 1 < derecha) {
+            QuickSort(n, d + 1, derecha);
+        }
+}
+
+//var arreglo = [17, 10, 12, 7 ,11];
+
+//console.log(QuickSort(arreglo));
+//se manda  allamar cualquier ordenamiento, con un array de parametro
+// y devuelve el arreglo ordenaod
+//se imprimen en consola todas las iteraciones para ver como se ordena la lista
