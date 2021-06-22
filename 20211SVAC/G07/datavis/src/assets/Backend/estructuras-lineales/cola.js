15a3c0e854aa772266cd776b5a93c0f7770e8600
Pablo2000Gt <70351807+Pablo2000Gt@users.noreply.github.com>
2021-06-21 22:07:40 -0600

    pila/cola

--- 20211SVAC/G07/datavis/src/assets/Backend/estructuras-lineales/cola.js
+++ 20211SVAC/G07/datavis/src/assets/Backend/estructuras-lineales/cola.js
@@ -1,28 +1,44 @@
-class cola {
-    constructor(){
-        this.queue = [];
-        this.indice = 0;
+class Node {
+    constructor(data, next){
+        this.data = data;
+        this.next = next;
     }
-
-    encolar(data){
-        this.queue[this.indice]=data;
-        this.indice++;
+  }
+  class Cola {
+   constructor() {
+       this.head = null;
+       this.size = 0;
+   }
+   add(data){
+       const newNode = new Node(data, null);
+       if ( !this.head ) {
+           this.head = newNode
+       }
+       else {
+           let actual = this.head
+           while (actual.next) {
+               actual = actual.next;
+           }
+           actual.next = newNode;
+       }
+       this.size++
+   }
+  
+   removeData(){
+    if (this.size<0){
+      return null;
+    }else{
+      let actual = this.head;
+      this.head = actual.next;
+      this.size--;
+      return actual.data;
     }
-
-    desencolar(){
-        return this.queue.shift();
-    }
-
-    peek(){
-        return this.queue[0];
-    }
-
-    empty(){
-        return this.indice===0;
-    }
-
-
-
-}
+  
+  
+  }
+  }
+  
+  
+  
 
 export default cola;
