e7fffebf0708f3065e9bb616f12430fbc375aab2
elytedc <70500580+elytedc@users.noreply.github.com>
2021-06-21 22:40:46 -0600

    arbol b complemento

--- 20211SVAC/G05/TytusDS/src/app/PaginasWeb/b/js/arbol_b.js
+++ 20211SVAC/G05/TytusDS/src/app/PaginasWeb/b/js/arbol_b.js
@@ -1,391 +1,211 @@
+
+
 class Nodo{
-    constructor(valor){
-        this.dato=valor;
-        this.post=null;
-        this.ant=null
-    }
+    constructor(padre, id){
+        this.id = id
+        this.claves = []
 
-}
+        this.hijos = []
 
-class cola{
-    constructor(){
-        this.uno=null;
-        this.fin=null;
-        this.tamaño=0;
-    }
-insertar(valor){
-    let nodo = new Nodo(valor)
-    nodo.dato=valor;
-    if(this.uno==null){
-        this.uno=nodo;
-        this.fin=nodo
-        }else{
-            nodo.post=this.uno;
-        this.uno=nodo;
-        }
-    this.tamaño++;
-    var element=document.createElement("button");
-        var myDiv=document.getElementById("myDiv1");
-          var content=document.createTextNode(valor);
-          element.appendChild(content);
-          element.style.backgroundColor='rgb(25, 25, 112)'; 
-          element.style.color='rgb(255,255,255)';
-          element.style.fontSize='15px'; 
-          element.style.borderRadius="5px";
-          element.style.boxShadow="0 9px black";
-          element.style.width="125px";
-          element.style.height="40px";
-          myDiv.insertBefore(element, myDiv.firstElementChild);
-          window.scroll({
-            top: 2000,
-            left: 100,
-            behavior: 'smooth'
-          });
-          
-          
-          var pos = -30;
-          var id = setInterval(frame, 4);
-    function frame() {
-        if (pos == 30) { clearInterval(id);} 
-        else {
-            pos++;
-            myDiv.style.top = pos + 'px';
-            myDiv.style.left =55 + 'px';
-        }
-    }
-    var element1=document.createElement("button");
-    var myDiv1=document.getElementById("myDiv1");
-    var content1=document.createTextNode("Encolar");
-    element1.appendChild(content1);
-    element1.style.backgroundColor='rgb(53,204,0)'; 
-    element1.style.color='rgb(255,255,255)';
-    element1.style.width="120px";
-    element1.style.height="30px";
-    myDiv1.insertBefore(element1, myDiv1.firstElementChild);
-    window.scroll({
-        top: 2000,
-        left: 100,
-        behavior: 'smooth'
-      });
-    var eli=function(){
-        var puntero=document.getElementById("myDiv1");
-        puntero.removeChild(puntero.childNodes[0]); 
-        };
-        setTimeout(eli,600);
-
-
-
-
-}
-
-insertar2(valor){
-let nodo = new Nodo(valor)
-nodo.dato=valor;
-if(this.uno==null){
-    this.uno=nodo;
-    this.fin=nodo
-    }else{
-        nodo.post=this.uno;
-    this.uno=nodo;
+        this.padre = padre}
     
-    }
-this.tamaño++;
+    agregar(valor){
+        this.claves.push(valor)
+        if(this.claves.length > 1){
 
+            var matriz=this.claves;
 
+            var aux = 0;
+        for(var i=0; i< matriz.length-1; i++){
+            for(var j=i+1; j<matriz.length; j++){
 
-
-}
-
-guardarg(valor) {
-    let aux = this.uno;
-    var bandera=true;
-    if(aux!=null){
-        do{if (valor==aux.dato){
-            bandera=false;
-            break;}
-            aux=aux.post;
-        }while(aux!=null);}
-
-        if (bandera==true){
-            let nodo = new Nodo(valor)
-            nodo.dato=valor;
-            if (this.uno==null){
-                this.uno=nodo;}
-            else{nodo.post=this.uno;
-                this.uno=nodo;}
-            this.tamaño++; 
-                var element=document.createElement("button");
-                var myDiv=document.getElementById("myDiv1");
-                  var content=document.createTextNode(valor);
-                  element.appendChild(content);
-                  element.style.backgroundColor='rgb(25, 25, 112)'; 
-                  element.style.color='rgb(255,255,255)';
-                  element.style.fontSize='15px'; 
-                  element.style.borderRadius="5px";
-                  element.style.boxShadow="0 9px black";
-                  element.style.width="125px";
-                  element.style.height="40px";
-                  myDiv.insertBefore(element, myDiv.firstElementChild);
-                  var element1=document.createElement("button");
-                  var myDiv1=document.getElementById("myDiv1");
-                  var content1=document.createTextNode(this.tamaño2);
-                  this.tamaño2++; 
-                  element1.appendChild(content1);
-                  element1.style.backgroundColor='rgb(0,0,0)'; 
-                  element1.style.color='rgb(255,255,255)';
-                  element1.style.width="30px";
-                  element1.style.height="30px";
-                  myDiv1.insertBefore(element1, myDiv1.firstElementChild);
-                  window.scroll({
-                    top: 2000,
-                    left: 100,
-                    behavior: 'smooth'
-                  });
-                  
-                  var pos = -30;
-                  var id = setInterval(frame, 4);
-            function frame() {
-                if (pos == 30) { clearInterval(id);} 
-                else {
-                    pos++;
-                    myDiv.style.top = pos + 'px';
-                    myDiv.style.left =55 + 'px';
+                if(matriz[i] > matriz[j]){
+                    aux = matriz[i];
+                    matriz[i] = matriz[j];
+                    matriz[j] = aux;
                 }
             }
-            var element1=document.createElement("button");
-            var myDiv1=document.getElementById("myDiv1");
-            var content1=document.createTextNode("PUSH");
-            element1.appendChild(content1);
-            element1.style.backgroundColor='rgb(53,204,0)'; 
-            element1.style.color='rgb(255,255,255)';
-            element1.style.width="70px";
-            element1.style.height="30px";
-            myDiv1.insertBefore(element1, myDiv1.firstElementChild);
-            window.scroll({
-                top: 2000,
-                left: 100,
-                behavior: 'smooth'
-              });
-            var eli=function(){
-                var puntero=document.getElementById("myDiv1");
-                puntero.removeChild(puntero.childNodes[0]); 
-                };
-                setTimeout(eli,600);
         }
-        else{
-            alert("dato no valido");
+            this.claves =matriz;
         }
-     }
+    }
 
 
-eliminar(){
-    let actual = this.uno;
-    let anterior = null;
-    if (this.uno!=null){
-        do{
-            if(actual==this.fin){
-                if(actual==this.uno){
-                  this.uno=this.uno.post;
+}
+
+var contador = 1
+
+var arrayNodes = []
+
+var edges = []
+
+var arbol=null;
+
+
+
+
+class ArbolB{
+
+    constructor(grado){
+
+        this.repetidos = false
+
+        this.grado = grado
+        if(this.g(grado)){
+            this.enmedio = grado/2
+        }else{
+            this.enmedio = (grado-1)/2
+        }
+        this.raiz = new Nodo(null, 0)
+    }
+
+
+    agregar(valor){
+        this.raiz = this.valueagregar(valor, this.raiz)
+    }
+
+    valueagregar(valor, temp){
+        if(temp.hijos.length == 0){
+            temp.agregar(valor)
+        }else{
+            var encontrar = false
+            for(var i = 0; i<temp.claves.length; i++){
+                if(valor < temp.claves[i]){
+
+                    encontrar = true
+                    temp.hijos[i] = this.valueagregar(valor, temp.hijos[i])
+                    break;
+                }
+            }
+            if(!encontrar){
+                temp.hijos[temp.claves.length] = this.valueagregar(valor, temp.hijos[temp.claves.length])
+            }
+        }
+        if(temp.claves.length == this.grado){
+            if(temp.padre == null){
+                contador++
+                var c = temp;
+                temp = new Nodo(null, contador)
+                temp.agregar(c.claves[this.enmedio])
+                contador++
+                temp.hijos.push(new Nodo(temp, contador))
+                contador++
+                temp.hijos.push(new Nodo(temp, contador))
+                for(var i = 0; i<this.enmedio; i++){
+                    temp.hijos[0].agregar(c.claves[i])
+                    temp.hijos[1].agregar(c.claves[i+this.enmedio+1])
+                }
+                if(c.hijos.length > 0){
+                    for(var i = 0; i<this.enmedio+1; i++){
+                        temp.hijos[0].hijos[i] = c.hijos[i]
+                        temp.hijos[0].hijos[i].padre = temp.hijos[0]
+                        temp.hijos[1].hijos[i] = c.hijos[i+this.enmedio+1]
+                        temp.hijos[1].hijos[i].padre = temp.hijos[1]
+                    }
+                }
             }else{
-              anterior.post=null;
-              this.fin=anterior;
-              }} 
-            anterior=actual;
-            actual=actual.post;
-            }while(actual!=null);
-            var puntero=document.getElementById("myDiv1");
-        if (puntero.childNodes.length>0) 
-            puntero.removeChild(puntero.childNodes[puntero.childNodes.length-1]);  
-            var element1=document.createElement("button");
-            var myDiv1=document.getElementById("myDiv1");
-            var content1=document.createTextNode("Desencolar");
-            element1.appendChild(content1);
-            element1.style.backgroundColor='rgb(213,0,36)'; 
-            element1.style.color='rgb(255,255,255)';
-            element1.style.width="120px";
-            element1.style.height="30px";
-            myDiv1.insertBefore(element1, myDiv1.firstElementChild);
-            window.scroll({
-                top: 2000,
-                left: 100,
-                behavior: 'smooth'
-              });
-            var eli=function(){
-                puntero.removeChild(puntero.childNodes[0]); 
-             };
-             setTimeout(eli,500);     
-             
+                var claveMedia = temp.claves[this.enmedio]
+                temp.padre.agregar(claveMedia)
+                var index
 
-
-    }
-    else{
-        alert("Cola sin datos");
-    }
-     }
-
-
-buscar(valor){
-    var ele= document.getElementById("myDiv1");
-    while (ele.firstChild) {
-        ele.removeChild(ele.firstChild);}   
-    let aux = this.uno;
-    var bandera=true;
-    var bandera1=true;
-    if (this.uno!=null){
-        //while(aux!=null){
-        
-        var fun=function(){
-                bandera1=false;
-                if (valor==aux.dato){
-                    bandera=false;
-                    var element=document.createElement("button");
-                    var myDiv=document.getElementById("myDiv1");
-                    var content =document.createTextNode(aux.dato);
-                    element.appendChild(content);
-                    element.style.backgroundColor='rgb(38,193,0)'; 
-                    element.style.color='rgb(255,255,255)';
-                    element.style.fontSize='10px'; 
-                    element.style.borderRadius="5px";
-                    element.style.boxShadow="0 9px black";
-                    element.style.width="125px";
-                    element.style.height="40px";
-                    myDiv.appendChild(element);
-                    window.scroll({
-                        top: 2000,
-                        left: 100,
-                        behavior: 'smooth'
-                      });
-                    //myDiv.insertBefore(element, myDiv.firstElementChild);
+                var tieneHijos = false
+                if(temp.hijos.length > 0){
+                    tieneHijos = true
                 }
-                else{
-                    var element=document.createElement("button");
-                    var myDiv=document.getElementById("myDiv1");
-                    var content =document.createTextNode(aux.dato);
-                    element.appendChild(content);
-                    element.style.backgroundColor='rgb(25, 25, 112)'; 
-                    element.style.color='rgb(255,255,255)';
-                    element.style.fontSize='10px'; 
-                    element.style.borderRadius="5px";
-                    element.style.boxShadow="0 9px black";
-                    element.style.width="125px";
-                    element.style.height="40px";
-                    myDiv.appendChild(element);
-                    window.scroll({
-                        top: 2000,
-                        left: 100,
-                        behavior: 'smooth'
-                      });
-                    //myDiv.insertBefore(element, myDiv.firstElementChild);
-                   
+
+
+                for(index = 0; index < temp.padre.claves.length; index++){
+                    if(temp.padre.claves[index] == claveMedia){
+                        break
+                    }
                 }
-                aux=aux.post;
-                    var pos = 0;
-                    var id = setInterval(frame, 10);
-    function frame() { if (pos == 50) { clearInterval(id);} 
-        else { pos++; myDiv.style.top = pos + 'px'; myDiv.style.left = 55 + 'px';}}
-              
-              if (aux==null){  
-                if(bandera1==false){ if (bandera==true){alert("dato no encontrado");}}
-                  clearInterval(intervalo);}
-            };
-        
-        var intervalo = setInterval(fun,1000);
-    }
-        else{
-            alert("Pila vacia"); 
+
+                for(var i = temp.padre.claves.length; i>index+1; i--){
+                    temp.padre.hijos[i] = temp.padre.hijos[i-1]
+                }
+
+                var aux = temp
+                contador++
+                temp.padre.hijos[index] = new Nodo(temp.padre, contador)
+                //console.log("Claves  "+temp.padre.hijos[index].claves)
+
+                contador ++
+                temp.padre.hijos[index+1] = new Nodo(temp.padre, contador)
+                for(var i = 0; i<this.enmedio; i++){
+                    console.log(aux.claves[i])
+                    temp.padre.hijos[index].agregar(aux.claves[i])
+                    temp.padre.hijos[index+1].agregar(aux.claves[i+this.enmedio+1])
+                }
+                temp = temp.padre.hijos[index]
+
+                if(tieneHijos){
+                    for(var i = 0; i<this.enmedio+1; i++){
+                        temp.padre.hijos[index].hijos[i] = aux.hijos[i]
+                        temp.padre.hijos[index].hijos[i].padre = temp.padre.hijos[index]
+                    }
+                    for(var i = this.enmedio+1; i<this.grado+1; i++){
+                        temp.padre.hijos[index+1].hijos[i-this.enmedio-1] = aux.hijos[i]
+                        temp.padre.hijos[index+1].hijos[i-this.enmedio-1].padre = temp.padre.hijos[index+1]
+                    }
+                    
+                }
+            }
         }
-     }
-    
-modificar(bus,valor){
-    let aux = this.uno;
-    var bandera=true;
-    if (this.uno!=null){
-        do{
-        if (bus==aux.dato){
-            console.log("mofificado: ");
-            alert("modificado");
-            aux.dato=valor;
-            bandera=false;
-            break;}
-        aux=aux.post;
-    }while(aux!=null);}
-    if (bandera==true){alert("no encontrado"); console.log("no encontrado");}
-}
-
-modificar2(bus,valor){
-    let aux = this.uno;
-    var bandera=true;
-    var bandera2=true;
-    if (this.uno!=null){
-        do{
-            if (valor==aux.dato){
-                bandera2=false;
-                console.log(valor);
-                console.log(aux.dato);
-                alert("dato ya existe");
-                bandera=false;
-                break;}
-            aux=aux.post;
-        }while(aux!=null);
-
-        if (bandera2==true){
-            aux = this.uno;
-            do{ 
-                if (bus==aux.dato){
-                console.log("mofificado: ");
-                alert("modificado");
-                aux.dato=valor;
-                bandera=false;}
-        aux=aux.post;}while(aux!=null);
+        return temp
     }
+
+    arbolgra(temp){
+        if(temp != null){
+            var texto = ""
+            var i
+            for(i = 0; i<temp.claves.length; i++){
+                if(i == temp.claves.length-1){
+                    texto = texto + temp.claves[i].toString();
+                }else{
+                    texto = texto + temp.claves[i].toString() + "|"
+                }
+                console.log(texto);
+            }
+            arrayNodes.push({id: temp.id, label: texto})
+            texto = ""
+            for(i = 0; i<temp.hijos.length; i++){
+                edges.push({from: temp.id, to: temp.hijos[i].id})
+                this.arbolgra(temp.hijos[i])
+            }
+        }
     }
-    if (bandera==true){alert("no encontrado"); console.log("no encontrado");}}
-
-
-
-leer(){
-    let ldatos=[];
-    let aux = this.uno;
-    if (this.uno!=null){
-        do{
-            ldatos.push(aux.dato.toString());
-            aux=aux.post;
-            }while(aux!=null);}
-    else{console.log("sin datos"); }
-    return ldatos;
-  }
-
-
-  pintar(){
-    var ele= document.getElementById("myDiv1");
-    while (ele.firstChild) {ele.removeChild(ele.firstChild);}   
-    let aux = this.uno;
-if (this.uno!=null){ do{
-    var element=document.createElement("button");
-    var myDiv=document.getElementById("myDiv1");
-    var content =document.createTextNode(aux.dato);
-    element.appendChild(content);
-    element.style.backgroundColor='rgb(25, 25, 112)'; 
-    element.style.color='rgb(255,255,255)';
-    element.style.fontSize='15px'; 
-    element.style.borderRadius="5px";
-    element.style.boxShadow="0 9px black";
-    element.style.width="125px";
-    element.style.height="40px";
-    //myDiv.insertBefore(element, myDiv.firstElementChild);
-    myDiv.appendChild(element);
-    window.scroll({
-        top: 2000,
-        left: 100,
-        behavior: 'smooth'
-      });
 
     
-    aux=aux.post; }while(aux!=null);}
-else{
-    alert("Pila vacia"); 
+     crearArbol(grado){
+        if(arbol == null){
+            arbol = new ArbolB(grado)
+        }
+         
+    }
+    
+    insertarNodo(valor){
+        arbol.agregar(valor)
+    }
+
+    as(){
+        arrayNodes = []
+        edges = []  
+        let ldata=[];
+        arbol.arbolgra(arbol.raiz);
+        ldata.push(arrayNodes);
+        ldata.push(edges);
+        return ldata;
+        
+    }
+
+  
+
+    g(valor){
+        if (valor%2==0) {return true;}
+        return false;
+ }
+
 }
-   }
 
 
 
@@ -393,21 +213,5 @@ else{
 
 
 
-
-imprimir(){
-    let aux = this.uno;
-    if (this.uno!=null){
-        do{
-            console.log(aux.dato);
-            aux=aux.post;
-            }while(aux!=null);
-    }
-    else{
-        console.log("sin datos");
-    }
-     }
-    }
-
-    module.exports = cola;
-
+module.exports = ArbolB;
 
