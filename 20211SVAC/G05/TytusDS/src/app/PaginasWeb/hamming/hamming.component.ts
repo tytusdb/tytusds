d8ab1f06306909be9392ef91a6a4132109dcd190
pereznator <3004272120101@ingenieria.usac.edu.gt>
2021-06-27 13:01:39 -0600

    hamming implementado

--- 20211SVAC/G05/TytusDS/src/app/PaginasWeb/hamming/hamming.component.ts
+++ 20211SVAC/G05/TytusDS/src/app/PaginasWeb/hamming/hamming.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CodigoHamming } from './ts/hamming';
 import { DocumentoService } from '../../services/documento.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-hamming',
@@ -19,6 +20,9 @@ export class HammingComponent implements OnInit {
     grado: 3,
     repeticionArboles: true,
   };
+
+  cargado = false;
+  subtitulos: any = [];
 
   entrada = '';
   salida = '';
@@ -51,19 +55,34 @@ export class HammingComponent implements OnInit {
     if (this.entrada.length === 0) {
       return;
     }
+    this.cargado = false;
     console.log(this.entrada);
     this.bits = this.ham.calcularBitsRedundantes(this.entrada.length);
     console.log(this.bits);
+    this.subtitulos = this.ham.getSubtitulos(this.entrada, this.bits);
+    console.log(this.subtitulos);
     let nuevaCadena = this.ham.rellenarPosiciones(this.entrada, this.bits);
     console.log(nuevaCadena);
     this.paridades = this.ham.calcularParidades(nuevaCadena, this.bits);
     console.log(this.paridades);
     this.salida = this.ham.calcularFinal(nuevaCadena, this.paridades);
     console.log(this.salida);
+    this.cargado = true;
+  }
+
+  elevarCuadrado(exponente: number): number {
+    return Math.pow(2, exponente);
   }
 
   guardar(): void {
-
+    const contenido: any = {
+      categoria: "Algoritmo de codificación",
+      nombre: "Código de Hamming",
+      entrada: this.entrada,
+      salida: this.salida
+    };
+    let blob = new Blob([JSON.stringify(contenido)], {type: 'json;charset=utf-8'});
+    saveAs(blob, 'hamming.json');
   }
 
 }
