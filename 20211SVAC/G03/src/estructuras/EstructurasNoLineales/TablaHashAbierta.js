52d984e584c7838863bc0c6310588bd093ae883f
Ariel Macario <arielmacario.11@gmail.com>
2021-06-28 17:21:54 -0600

    tabla hash abierta, funcion hash

--- 20211SVAC/G03/src/estructuras/EstructurasNoLineales/TablaHashAbierta.js
+++ 20211SVAC/G03/src/estructuras/EstructurasNoLineales/TablaHashAbierta.js
@@ -21,4 +21,52 @@ class TablaHashAbierta {
         this.iniciar();
         
     }
+
+    iniciar(){
+        let newTable = new Array(this.tamaño)
+
+        for (let index = 0; index < this.tamaño; index++) {
+            newTable[index] = -1
+            
+        }
+
+        this.tabla = newTable
+    }
+
+    funcion_Hash(dato, forma){
+        let valorretorno;
+        switch(forma){
+            
+            case "Hash Simple":
+                valorretorno = this.hashSimple(dato)
+                break;
+            case "Metodo Division":
+                valorretorno =  this.division(dato)
+                break;
+            case "Metodo Multiplicación":
+                valorretorno =  this.multiplicacion(dato)
+                break;
+            default:
+                break;
+
+        }
+
+        return valorretorno
+    }
+
+    hashSimple(valor){
+        let hash = valor * this.tabla.length
+    }
+
+    division(valor){
+        let hash = valor % this.tabla.length
+
+        return hash
+    }
+
+    multiplicacion(valor){
+        let hash = (this.tabla.length(valor*0.2520 % 1))
+
+        return hash
+    }
 }
