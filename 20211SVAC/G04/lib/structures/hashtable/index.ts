8afe0193748388be15f25822b85c3666d246c6d7
Walter Alexander Guerra Duque <39949921+AlexIngGuerra@users.noreply.github.com>
2021-06-28 21:47:19 -0600

    [Add] Implementacion Tabla Hash Abierta

--- 20211SVAC/G04/lib/structures/hashtable/index.ts
+++ 20211SVAC/G04/lib/structures/hashtable/index.ts
@@ -59,3 +59,87 @@ class FuncionHash{
 
 }
 
+class NodoHashAbierto{
+    clave:number
+    valores:Tupla[]
+
+    constructor(clave:number){
+        this.clave = clave
+        this.valores = []
+    }
+
+    eliminarTupla(valor:any){
+        let tupla = this.buscarTupla(valor)
+        if(tupla != null){
+            this.valores = this.deleteFromArray(this.valores, tupla)
+            return true
+        }
+        return false
+    }
+
+    private buscarTupla(valor:any){
+        for(let i of this.valores){
+            if(i.valor  == valor){
+                return i
+            }
+        }
+        return null
+    }
+
+    private deleteFromArray(arreglo:any, valor:any){
+        return arreglo.filter( function( e:any ) {
+            return e !== valor;
+        } );
+    }
+
+}
+
+class TablaHashAbierta{
+    tamaño:number
+    funcion:FuncionHash
+    tabla:NodoHashAbierto[]
+
+    constructor(tamaño:number, tipoFuncion:number){
+        this.tamaño = tamaño
+        this.funcion = new FuncionHash(tipoFuncion)
+        this.tabla = []
+        this.crearTabla()
+    }
+
+    crearTabla(){
+        for (let i = 0; i < this.tamaño; i++) {
+            this.tabla.push(new NodoHashAbierto(i))
+        }
+    }
+
+    insertar(valor:any){
+        //Obtiene la posicion del arreglo
+        let clave = this.funcion.funcionHash(valor, this.tamaño)
+        //Le da al arreglo un valor
+        if(this.tabla[clave].clave == -1)
+            this.tabla[clave].clave = clave
+        //Ingresa el valor al arreglo
+        this.tabla[clave].valores.unshift(new Tupla(this.funcion.stringToAscii(valor), valor))
+    }
+
+    eliminar(valor:any){
+        let clave = this.funcion.funcionHash(valor, this.tamaño)
+        this.tabla[clave].eliminarTupla(valor)
+    }
+
+    actualizar(valor:any){
+        let clave = this.funcion.funcionHash(valor, this.tamaño)
+        if(this.tabla[clave].eliminarTupla(valor)){
+            this.insertar(valor)
+        }
+    }
+
+    print(){
+        for (let i = 0; i < this.tabla.length; i++) {
+            console.log('Posicion: '+i + ' - Clave: '+this.tabla[i].clave)
+            console.log(this.tabla[i].valores)
+        }
+    }
+
+
+}
