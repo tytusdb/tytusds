534bf864c67835161648a7700724c2d70f9d33d7
Mario <3005659920101@ingenieria.usac.edu.gt>
2021-06-29 16:58:58 -0600

    ListaAdyacenciaRecorridoAnchura

---
+++ 20211SVAC/G03/src/estructuras/EstructurasNoLineales/Grafo2.js
@@ -0,0 +1,68 @@
+var idNodos = 0
+
+class ListaDoble{
+    constructor(){
+        this.cabeza = null
+        this.cola = null
+        this.size = 0
+    }
+
+    insertar(dato){
+        if(this.cabeza == null){
+            let nodo = new NodoListaDoble(dato)
+            this.cabeza = nodo
+            this.cola = nodo
+            this.size++
+        }else{
+            let nodo = new NodoListaDoble(dato)
+            let aux = this.cabeza
+            while(aux.siguiente != null){
+                aux = aux.siguiente
+            }
+            aux.siguiente = nodo
+            nodo.anterior = aux
+            this.cola = nodo
+            this.size++
+        }
+    }
+
+    eliminar(dato){
+        let aux = this.cabeza
+        while(aux != null){
+            if(dato == aux.dato && aux == this.cabeza){
+                let tmp = this.cabeza.siguiente
+                tmp.anterior = null
+                this.cabeza.siguiente = null
+                this.cabeza = tmp          
+                this.size--      
+                return
+            }else if(dato == aux.dato && aux.anterior != null && aux.siguiente != null){
+                let tmp = aux.siguiente
+                tmp.anterior = aux.anterior
+                aux.anterior.siguiente = tmp
+                aux.siguiente = null
+                aux.anterior = null
+                this.size--
+                return
+            }else if(dato == aux.dato && aux == this.cola){
+                let tmp = this.cola.anterior
+                tmp.siguiente = null
+                this.cola.anterior = null
+                this.cola = tmp
+                this.size--
+                return
+            }else if(dato == aux.dato && this.cola == this.cabeza){
+                this.cola = null
+                this.cabeza = null
+                this.size--
+                return
+            }
+            aux = aux.siguiente
+        }
+    }
+}
+
+
+
+
+
