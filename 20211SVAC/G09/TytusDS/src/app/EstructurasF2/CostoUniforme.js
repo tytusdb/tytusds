771506d1bceb735d6a1fc690241657aa87df707c
Julissa <mjuligm1996@gmail.com>
2021-06-29 18:41:54 -0600

    Costo Uniforme Funcional

--- 20211SVAC/G09/TytusDS/src/app/EstructurasF2/CostoUniforme.js
+++ 20211SVAC/G09/TytusDS/src/app/EstructurasF2/CostoUniforme.js
@@ -1,158 +1,65 @@
-function CrearGrafo (numNodes, edgeProbability, maxWeight) {
-  var g = []
-  for (var a = 0; a < numNodes; ++a) {
-    g[a] = []
+function CostoUniforme(Grafo,NodoInicial,NodoFinal){
+  //Grafo
+  this.Grafo = Grafo;
+  //Nodo Inicial
+  this.NodoInicial = NodoInicial;
+  //Nodo Final
+  this.NodoFinal = NodoFinal;
+
+  let lowestPaths=Object.assign({[NodoFinal]:Infinity},Grafo[NodoInicial])
+  let lowestPathsParents={[NodoFinal]:null}
+  for(let element in Grafo[NodoInicial]){
+    lowestPathsParents[element]=NodoInicial;
   }
-  for (var i = 0; i < numNodes; ++i) {
-    for (var j = i + 1; j < numNodes; ++j) {
-      var randomNumber = Math.random()
-      var length = Number.POSITIVE_INFINITY
-      if (randomNumber > edgeProbability) {
-        length = Math.floor(Math.random() * maxWeight)
-      }
-      g[i][j] = g[j][i] = length
+  //Nodos Visitados
+  let NodosVisitados=[]
+  //Nodos
+  let node;
+  node = findCheapest(lowestPaths,NodosVisitados)
+    while(node){
+        let path = lowestPaths[node];
+        let NodosHijos = Grafo[node]
+        for(let NodoHijo in NodosHijos){
+            let newPath = path + NodosHijos[NodoHijo]
+            if(!lowestPaths[NodoHijo]||lowestPaths[NodoHijo]>newPath){
+                lowestPaths[NodoHijo]= newPath
+                lowestPathsParents[NodoHijo]= node;
+            }
+        }
+        NodosVisitados.push(node)
+        node = findCheapest(lowestPaths,NodosVisitados)
     }
+  let finalAnswer = [NodoFinal];
+  let parent = lowestPathsParents[NodoFinal]
+  while(parent!=NodoInicial){
+      finalAnswer.unshift(parent)
+      parent=lowestPathsParents[parent]
   }
-  return g
+  finalAnswer.unshift(NodoInicial)
+  let MejorCamino ={
+      Camino: finalAnswer,
+      Costo: lowestPaths[NodoFinal]
+  }
+  return MejorCamino 
 }
 
-
-function CostoUniforme (Grafo, NodoInicial, NodoFinal) {
-    // Grafo
-    this.Grafo = Grafo
-    // Nodo inicial
-    this.NodoInicial = NodoInicial
-    // Nodo final
-    this.NodoFinal = NodoFinal
-    // Costo de cada nodo
-    this.costoNodo = []
-    // Costo de nodo raíz
-    this.costoNodo[NodoInicial] = 0
-    // Nodo previo
-    this.previous = []
-    // Objeto con los nodos marcados como visitados
-    this.NodosVisitados = {}
-    // Objeto que nos permitira el ordenamiento de los nodos
-    this.OrdenarNodo = new MinHeap()
-    for (var v = 0; v < Grafo.length; ++v) {
-      (v !== NodoInicial)
-      ? this.costoNodo[v] = Number.POSITIVE_INFINITY
-      : this.costoNodo[v]
-      this.OrdenarNodo.insert(v, this.costoNodo[v])
-    }
-    // Nodo padre
-    this.NodoPadre = undefined
-    // Numero de nodos del padre
-    this.NodosHijos = undefined
-    // Ultimo nodo abierto
-    this.R = undefined
-    // Nodo hijo actual
-    this.HijoActual = undefined
-    // Falso mientras no se encuentre el nodo objetivo
-    this.complete = false
+function findCheapest(lowestPaths,NodosVisitados){
+    let nodes=Object.keys(lowestPaths)
+    let lowestNodes=nodes.reduce((lowest,node)=>{
+        if(lowest==null&&!NodosVisitados.includes(node)) lowest=node;
+        if(lowestPaths[lowest]>lowestPaths[node]&&!NodosVisitados.includes(node)) lowest=node;
+        return lowest
+    },null)
+    return lowestNodes
+}
+  let Grafo={
+    H: { M: 50, A: 30, O: 40},
+    M: { A: 71, W: 33},
+    A: { H: 30, O: 22},
+    W: { A: 45,O: 65},
+    C: {W: 65},
+    O: {A: 22, F: 32},
+    F: { H: 60, K: 77 },
+    K: { W: 28, F: 77}
   }
-  
-  CostoUniforme.prototype = {
-    constructor: CostoUniforme,
-    // Función de cada paso del algoritmo
-    step: function () {
-      if (this.complete) {
-        return true
-      }
-      // Asignación de nodo padre
-      if (this.NodoPadre === undefined) {
-        /* Validación del nodo que su costo no sea 0,
-         valor infinito o sea un estado objetivo. */
-        if (this.OrdenarNodo.size() === 0 ||
-            this.OrdenarNodo.peekMin().key === Number.POSITIVE_INFINITY ||
-            this.OrdenarNodo.peekMin().value === this.NodoFinal
-        ) {
-          // Si alguna se cumple el estado de busqueda finaliza
-          this.complete = true
-          return true
-        }
-        // Se asigna el nodo con menor costo
-        this.NodoPadre = this.OrdenarNodo.extractMin().value
-        // Marcar nodo padre como visitado
-        this.NodosVisitados[this.NodoPadre] = true
-        // Valor numero de nodos del padre sin definir
-        this.NodosHijos = undefined
-        // Ultimo nodo abierto no definido
-        this.R = undefined
-        // Nodos hijo actual no definido
-        this.HijoActual = undefined
-        // La busqueda debe continuar
-        return false
-      }
-      if (this.NodosHijos === undefined) {
-        // Creación de numero de nodos hijos
-        this.NodosHijos = []
-        // Validación que el nodo sea correcto
-        for (var R = 0; R < this.Grafo.length; ++R) {
-          if (R in this.NodosVisitados || R === this.NodoPadre ||
-            this.Grafo[this.NodoPadre][R] === Number.POSITIVE_INFINITY
-          ) {
-            continue
-          }
-          // Agregamos los nodos hijos
-          this.NodosHijos.push(R)
-        }
-        // Grafo aún no finalizado
-        return false
-      }
-      // visitar todos los nodos hijos
-      (this.HijoActual === undefined)
-      // no esta definido se asigna el nodo 0
-      ? this.HijoActual = 0
-      // es distinto se agrega 1
-      : this.HijoActual += 1
-      // En caso de que nodo hijo sea ultimo
-      if (this.HijoActual === this.NodosHijos.length) {
-        // Nodo padre igual a no definido
-        this.NodoPadre = undefined
-        // nodos padre no definido
-        this.NodosHijos = undefined
-        // Estado aún no finalizado
-        return false
-      }
-      // Se asigna nodo abierto
-      this.R = this.NodosHijos[this.HijoActual]
-      // Se calcula el costo del nodo padre al nodo hijo visitado
-      var costo = this.costoNodo[this.NodoPadre] + this.Grafo[this.NodoPadre][this.R]
-      // Validación que el nodo sea el de menor costo
-      if (costo < this.costoNodo[this.R]) {
-        this.costoNodo[this.R] = costo
-        this.previous[this.R] = this.NodoPadre
-        this.OrdenarNodo.decreaseKey(this.R, costo)
-      }
-      // Estado del grafo aún sin terminar
-      return false
-    },
-    
-    // Función para encontrar el camino mas corto
-    shortestPath: function () {
-      var nodes = []
-      var edges = []
-      // Si nodo el valor del nodo final es distinto a infinito
-      if (this.costoNodo[this.NodoFinal] !== Number.POSITIVE_INFINITY) {
-        var NodoAuxiliar = this.NodoFinal
-        nodes.push(NodoAuxiliar)
-        while (true) {
-          // Recorremos del nodo objetivo al nodo raíz
-          var prevNode = this.previous[NodoAuxiliar]
-          edges.push({src: NodoAuxiliar, dst: prevNode})
-          NodoAuxiliar = prevNode
-          nodes.push(prevNode)
-          if (NodoAuxiliar === this.NodoInicial) {
-            // si el nodo actual es nodo raíz finalizar while
-            break
-          }
-        }
-      }
-      // Regresamos todas las aristas y los nodos asi como la distancia
-      return {edges: edges, nodes: nodes, distance: this.costoNodo[this.NodoFinal]}
-    }
-  }
-
-  var a = new CostoUniforme(CrearGrafo, 1, 10);
-  console.log(a);
+  console.log(CostoUniforme(Grafo, 'H', 'K'));
