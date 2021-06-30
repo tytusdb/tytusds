f0d208a72acda51e8fdf560ce6836f5a8abf568f
Julissa <mjuligm1996@gmail.com>
2021-06-29 19:29:10 -0600

    Costo Uniforme

--- 20211SVAC/G09/TytusDS/src/app/EstructurasF2/CostoUniforme.js
+++ 20211SVAC/G09/TytusDS/src/app/EstructurasF2/CostoUniforme.js
@@ -6,51 +6,52 @@ function CostoUniforme(Grafo,NodoInicial,NodoFinal){
   //Nodo Final
   this.NodoFinal = NodoFinal;
 
-  let lowestPaths=Object.assign({[NodoFinal]:Infinity},Grafo[NodoInicial])
-  let lowestPathsParents={[NodoFinal]:null}
+  let CaminoCorto=Object.assign({[NodoFinal]:Infinity},Grafo[NodoInicial])
+  let CaminoCortoNodosPadres={[NodoFinal]:null}
   for(let element in Grafo[NodoInicial]){
-    lowestPathsParents[element]=NodoInicial;
+    CaminoCortoNodosPadres[element]=NodoInicial;
   }
   //Nodos Visitados
   let NodosVisitados=[]
   //Nodos
-  let node;
-  node = findCheapest(lowestPaths,NodosVisitados)
-    while(node){
-        let path = lowestPaths[node];
-        let NodosHijos = Grafo[node]
+  let Nodo;
+  Nodo = findCheapest(CaminoCorto,NodosVisitados)
+    while(Nodo){
+        let Camino = CaminoCorto[Nodo];
+        let NodosHijos = Grafo[Nodo]
         for(let NodoHijo in NodosHijos){
-            let newPath = path + NodosHijos[NodoHijo]
-            if(!lowestPaths[NodoHijo]||lowestPaths[NodoHijo]>newPath){
-                lowestPaths[NodoHijo]= newPath
-                lowestPathsParents[NodoHijo]= node;
+            let CaminoNuevo; 
+            CaminoNuevo = Camino + NodosHijos[NodoHijo]
+            if(!CaminoCorto[NodoHijo]||CaminoCorto[NodoHijo]>CaminoNuevo){
+                CaminoCorto[NodoHijo]= CaminoNuevo
+                CaminoCortoNodosPadres[NodoHijo]= Nodo;
             }
         }
-        NodosVisitados.push(node)
-        node = findCheapest(lowestPaths,NodosVisitados)
+        NodosVisitados.push(Nodo)
+        Nodo = findCheapest(CaminoCorto,NodosVisitados)
     }
   let finalAnswer = [NodoFinal];
-  let parent = lowestPathsParents[NodoFinal]
+  let parent = CaminoCortoNodosPadres[NodoFinal]
   while(parent!=NodoInicial){
       finalAnswer.unshift(parent)
-      parent=lowestPathsParents[parent]
+      parent=CaminoCortoNodosPadres[parent]
   }
   finalAnswer.unshift(NodoInicial)
   let MejorCamino ={
       Camino: finalAnswer,
-      Costo: lowestPaths[NodoFinal]
+      Costo: CaminoCorto[NodoFinal]
   }
   return MejorCamino 
 }
 
-function findCheapest(lowestPaths,NodosVisitados){
-    let nodes=Object.keys(lowestPaths)
-    let lowestNodes=nodes.reduce((lowest,node)=>{
-        if(lowest==null&&!NodosVisitados.includes(node)) lowest=node;
-        if(lowestPaths[lowest]>lowestPaths[node]&&!NodosVisitados.includes(node)) lowest=node;
+function findCheapest(CaminoCorto,NodosVisitados){
+    let Nodos=Object.keys(CaminoCorto)
+    let NodoAuxiliar=Nodos.reduce((lowest,Nodo)=>{
+        if(lowest==null&&!NodosVisitados.includes(Nodo)) lowest=Nodo;
+        if(CaminoCorto[lowest]>CaminoCorto[Nodo]&&!NodosVisitados.includes(Nodo)) lowest=Nodo;
         return lowest
     },null)
-    return lowestNodes
+    return NodoAuxiliar
 }
   let Grafo={
     H: { M: 50, A: 30, O: 40},
