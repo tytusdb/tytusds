ff1b960d6f2569c57045360948ff30ebf9325ab1
Victor Cuches de León <69135983+VictorCuches@users.noreply.github.com>
2021-07-01 01:12:33 -0600

    Animacion Feistel

--- 20211SVAC/G08/js/codificacion/feistel.js
+++ 20211SVAC/G08/js/codificacion/feistel.js
@@ -1,9 +1,14 @@
 
 // E  V  E  N  T  O  S
+/*
+// ----- OBTENER LLAVE -----
+var btn_Key = document.getElementById("addLlave");
+btn_Key.addEventListener("click", saveKey);
+
 // ----- OBTENER NUM DE RONDAS -----
 var btn_Rondas = document.getElementById("addRonda");
 btn_Rondas.addEventListener("click", rondaInfo);
-
+*/
 // ----- VELOCIDAD -----
 var btn_Velocidad = document.getElementById("velocidad");
 btn_Velocidad.addEventListener("click", getVelocidad)
@@ -23,12 +28,25 @@ btn_Clear.addEventListener("click", limpiar)
 // ----- TEXTAREA -----
 var spc_Text = document.getElementById("espacioTxt");
 
+// ----- CUADROS PARA ANIMACION -----
+var izqui = document.getElementById("left");
+var dere = document.getElementById("right");
+var func = document.getElementById("func");
+var llavero = document.getElementById("llavero");
+var resultado = document.getElementById("respuesta");
+
 // V  A  R  I  A  B  L  E  S  -  G  L  O  B  A  L  E  S
 var contenido;
 var rondas;
+var velocidad;
+
 
 // F  U  N  C  I  O  N  E  S  -  E  V  E  N  T  O  S
+// ****** LLAVE *****
+function saveKey(){
+    key = document.getElementById("llave").value;
 
+}
 // ****** NUMERO DE RONDAS *****
 function rondaInfo(){
     rondas = document.getElementById("rondas").value;
@@ -40,11 +58,15 @@ function codificar(){
     let leftSide = [];
     let rightSide = [];
     console.log("Codificando");
+    
     // obteniendo contenido del textarea por si se modifica algo
     var texto = document.getElementById("espacioTxt").value;
-    var prueba = document.getElementById("espacioTxt2");
+    var key = document.getElementById("llave").value;
+    rondas = parseInt(document.getElementById("rondas").value);
+    
+    console.log(texto.length)
     // LA LLAVE SERA POR EL MOMENTO IGUAL A 1 - PENDIENTE DE CAMBIOS
-    var llave = llaveBin(convertSB("1").split(""));
+    var llave = llaveBin(convertSB(key).split(""));
     var charBin = convertSB(texto); // lista de caracteres en binario
     console.log(charBin+" - "+llave);
     console.log("rondas: "+parseInt(rondas))
@@ -58,31 +80,83 @@ function codificar(){
             rightSide.push(parseInt(charBin[i]));
         }
     }
+    if(leftSide.length > llave.length){
+
+        llave = addZero(llave, leftSide.length);
+
+    }
     console.log(leftSide);
     console.log(rightSide);
     console.log(llave);
     console.log("Aplicando Feistel")
     feistel(leftSide, rightSide, llave);
-    prueba.textContent = charBin;
+    
+    izqui.textContent =leftSide.join("");
+    dere.textContent =rightSide.join("");
+    llavero.textContent = llave.join("");
+    /*
+    await new Promise((resolve) =>
+                setTimeout(() =>{
+                    resolve();
+                }, (velocidad*200)) //delay
+            );
+    */
 }
 
 // ***** CIFRADO FEISTEL *****
-function feistel(L, R, key){
+async function feistel(L, R, key){
     let contador = 0;
     let der = [];
     let temp = [];
-    
-    
+    velocidad = 10;
     while(contador < parseInt(rondas)){
         console.log("ITERACION W"+contador)
+        
+        dere.textContent = R.join("");
+        llavero.textContent = key.join("");
+        dere.style.backgroundColor = "#FF4949";
+        llavero.style.backgroundColor = "#FF4949";
+
+        await new Promise((resolve) =>
+        setTimeout(() =>{
+            resolve();
+        }, (velocidad*200)) //delay
+        );
 
         for(let i = 0; i < R.length; i++){
             temp.push(R[i] ^ key[i])
         } //01110111 //01010110
+        
+        func.style.backgroundColor = "#29C1F5";
+        func.textContent = temp.join("");
+        await new Promise((resolve) =>
+        setTimeout(() =>{
+            resolve();
+        }, (velocidad*200)) //delay
+        );
+        dere.style.backgroundColor = "#FFFFFF";
+        llavero.style.backgroundColor = "#FFFFFF";
+        func.style.backgroundColor = "#FF4949";
+        izqui.style.backgroundColor = "#FF4949";
+        await new Promise((resolve) =>
+        setTimeout(() =>{
+            resolve();
+        }, (velocidad*200)) //delay
+        );
         console.log(temp);
         for(let j = 0; j < L.length; j++){
             der.push(L[j] ^ temp[j])
         } //00110100
+        dere.style.backgroundColor = "#29C1F5";
+        dere.textContent = der.join("");
+        await new Promise((resolve) =>
+        setTimeout(() =>{
+            resolve();
+        }, (velocidad*200)) //delay
+        );
+        func.style.backgroundColor = "#FFFFFF";
+        izqui.style.backgroundColor = "#FFFFFF";
+        dere.style.backgroundColor = "#FFFFFF";
         console.log(der);
         L = R; //01000110
         console.log(L);
@@ -95,6 +169,17 @@ function feistel(L, R, key){
         key.push(ultimo);
         
         console.log(key);
+        izqui.style.backgroundColor = "#E2F240";
+        izqui.textContent = L.join("");
+        llavero.style.backgroundColor = "#E2F240";
+        llavero.textContent = key.join("");
+        await new Promise((resolve) =>
+        setTimeout(() =>{
+            resolve();
+        }, (velocidad*200)) //delay
+        );
+        izqui.style.backgroundColor = "#FFFFFF";
+        llavero.style.backgroundColor = "#FFFFFF";
 
       
         contador++;
@@ -103,7 +188,10 @@ function feistel(L, R, key){
     console.log("RESULTADO FINAL")
     console.log(R);
     let allBin = L.join("") + R.join("");
+    var codeF = convertBS(allBin);
     console.log(convertBS(allBin));
+    resultado.textContent = codeF;
+
 }
 
 // ***** CONVERTIR EL CONTENIDO A BINARIO *****
@@ -134,7 +222,14 @@ function llaveBin(content){
         newList.push(parseInt(content[i]))
     }
     return newList;
+}
 
+// ***** EMPAREJAR LLAVE *****
+function addZero(lKey, size){
+    while(lKey.length < size){
+        lKey.unshift(0);
+    }
+    return lKey;
 }
 // ***** LIMPIAR PANTALLA *****
 function limpiar(){
@@ -145,8 +240,8 @@ function limpiar(){
 // ***** CAMBIAR VELOCIDAD *****
 function getVelocidad(){
     console.log("Acelerando");
-    var spc_Text = document.getElementById("espacioTxt").value;
-    console.log(spc_Text)
+    velocidad = document.getElementById("numVelocidad").value;
+   
 
 }
 
