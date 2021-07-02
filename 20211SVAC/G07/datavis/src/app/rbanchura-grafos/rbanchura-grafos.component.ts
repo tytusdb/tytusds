1198f0c7066838231c498f5f773d04f31585af5d
Pablo2000Gt <70351807+Pablo2000Gt@users.noreply.github.com>
2021-07-02 02:14:10 -0600

    update corregido y clases

--- 20211SVAC/G07/datavis/src/app/rbanchura-grafos/rbanchura-grafos.component.ts
+++ 20211SVAC/G07/datavis/src/app/rbanchura-grafos/rbanchura-grafos.component.ts
@@ -12,13 +12,26 @@ let listaData = { nodes: nodes,
   edges: edges };
 
 
+  // class arista {
+  //   from: vertice;
+  //   peso: number;
+  //   to: vertice;
+  //   constructor(from,peso,to) {
+  //     this.from = from;
+  //     this.peso = peso;
+  //     this.to = to;
+  //   }
+  // }
+
   class vertice {
+    id: number;
     valor: any;
     adyacencia: vertice[];
     aristas: number[];
 
-    constructor(value) {
+    constructor(value, id) {
       this.valor=value;
+      this.id = id;
     }
 
     addArista(peso,vertice){
@@ -26,6 +39,7 @@ let listaData = { nodes: nodes,
       this.adyacencia.push(vertice);
     }
   }
+
   class grafo {
     vertix: vertice[];
     constructor() {
@@ -81,25 +95,34 @@ export class RBAnchuraGrafosComponent implements OnInit {
     nodes.add(
       {id: this.ValorPos(data), label: data}
     );
-    pagina.vertix.push(new vertice(data));
+    pagina.vertix.push(new vertice(data,this.ValorPos(data)));
   }
 
   addArista(veri,dist,verf){
     edges.update(
       {from: this.ValorPos(veri), to: this.ValorPos(verf) , length:20, label:dist}
     );
+
+    var tmp = this.buscarPagina(veri);
+    tmp.aristas.push(dist);
+    tmp.adyacencia.push(verf);
   }
+
   deleteVertice(data){
     nodes.remove(this.ValorPos(data));
+    pagina.vertix.splice(this.getindexPagina(data),1)
   }
   actualizarVertice(viejo, nuevo){
+    var tmp = this.buscarPagina(viejo);
     nodes.update({
-      id: this.ValorPos(viejo), label: nuevo
+      id: tmp.id, label: nuevo
     });
+    tmp.valor = nuevo;
   }
   buscarVertice(data){
+    var tmp = this.buscarPagina(data);
     nodes.update(
-      {id: this.ValorPos(data), color: "red"}
+      {id: tmp.id, color: "red"}
     );
   }
 
@@ -112,7 +135,20 @@ export class RBAnchuraGrafosComponent implements OnInit {
   
   descargar(){}
 
-
+  buscarPagina(data){
+    for(var i = 0;i<pagina.vertix.length;i++){
+      if(pagina.vertix[i].valor==data){
+        return pagina.vertix[i];
+      }
+    }
+  }
+  getindexPagina(data){
+    for(var i = 0;i<pagina.vertix.length;i++){
+      if(pagina.vertix[i].valor==data){
+        return i;
+      }
+    }
+  }
   
   abrir(eve:any)
   {
