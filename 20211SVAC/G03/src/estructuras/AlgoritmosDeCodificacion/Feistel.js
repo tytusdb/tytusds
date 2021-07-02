b0b4f7d321e10bc5d0e182ce766933c6b79a188a
Erick Mayorga <emayorga30@gmail.com>
2021-07-02 16:10:24 -0600

    Clase Feistel

---
+++ 20211SVAC/G03/src/estructuras/AlgoritmosDeCodificacion/Feistel.js
@@ -0,0 +1,12 @@
+class Feistel{
+    constructor(){
+        this.entrada = null;
+        this.salida = null;
+        this.tablaIzquierda = []
+        this.tablaDerecha = [];   
+    }
+
+
+
+
+}
