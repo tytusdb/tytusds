bafde173f69259cc5830918e2cdd450b0e5e7a4e
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2021-07-02 13:03:37 -0600

    Feistel2

--- 20211SVAC/G05/TytusDS/src/app/PaginasWeb/feistel/feistel.component.ts
+++ 20211SVAC/G05/TytusDS/src/app/PaginasWeb/feistel/feistel.component.ts
@@ -2,14 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { saveAs } from 'file-saver';
 import { DocumentoService } from '../../services/documento.service';
 declare var require: any;
+let Feistel=require('./js/feistel');
 @Component({
   selector: 'app-feistel',
   templateUrl: './feistel.component.html',
   styleUrls: ['./feistel.component.css']
 })
 export class FeistelComponent implements OnInit {
+  feistel=Feistel;
   resultado="";
   T_comprimir="";
+  cabecera=["IZQUIERDA","DERECHA"];
+  mitad=0;
+  key='';
+  iteraciones=[];
+  Niteraciones=0;
+  eIteraciones=false;
   opciones = {
     ingreso: 'final',
     velocidadLineales: 1000,
@@ -22,10 +30,20 @@ export class FeistelComponent implements OnInit {
   constructor(private documentoService: DocumentoService) { }
 
   ngOnInit(): void {
+    this.feistel=new Feistel('','',0);
   }
   ///CAMBIO DE OPCIONES--------------------------------------
   getOpciones(opciones: any): void {
     this.opciones = opciones;
+  }
+  //CIFRAR
+  cifrar(Niteracion){
+    this.eIteraciones=true;
+    this.feistel.IngDatos(this.T_comprimir,this.key,Niteracion);
+    this.feistel.Cifrar();
+    this.resultado=this.feistel.resultado;
+    this.iteraciones=this.feistel.iteraciones;
+    this.mitad=this.resultado.length/2;
   }
 
   //LEER ARCHIVOS DE ENTRADA--------------------------------
@@ -39,7 +57,5 @@ export class FeistelComponent implements OnInit {
     }
   }
   guardar(){}
-  cifrar(valor){
 
-  }
 }
