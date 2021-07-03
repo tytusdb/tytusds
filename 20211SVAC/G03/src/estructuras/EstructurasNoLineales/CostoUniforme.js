7dac94d337e7fee13affedff018a099cb9ea1b8c
Erick Mayorga <emayorga30@gmail.com>
2021-07-03 07:55:18 -0600

    Cargar Guardar y Graficar

--- 20211SVAC/G03/src/estructuras/EstructurasNoLineales/CostoUniforme.js
+++ 20211SVAC/G03/src/estructuras/EstructurasNoLineales/CostoUniforme.js
@@ -525,7 +525,62 @@ class ListaAdyacencia{
         }        
     }
 
+  
 
+    //Metodo Carga
+    cargar(arreglo) {
+        arreglo.array.map(vertice, arista, distancia => {
+            this.insert(vertice, vertice, arista, distancia)
+        })
+    }
+
+    //Metodo Guardar
+    guardar() {
+        let archivojs = [];
+        let aux = this.ListaAdyacencia.cabeza
+        while (aux != null){
+            archivojs.push("vertice")
+            archivojs.push(aux.dato.dato)
+            let tmp = aux.dato.enlaces.cabeza
+            while(tmp!=null){
+                archivojs.push("arista")
+                archivojs.push(tmp.dato.destino.dato)
+                archivojs.push("distancia")
+                archivojs.push(tmp.dato.distancia)            
+                tmp=tmp.siguiente
+            }       
+            aux=aux.siguiente
+        }
+        let json = JSON.stringify(archivojs)
+        let nombre = "CostoMinimo"
+        fs.writeFile(nombre, json)  
+    }
+
+    //Carga de array para graficacion de nodos
+    graficarNodos(){
+        let aux = this.ListaAdyacencia.cabeza
+        while(aux!=null){
+            let dato = {id: aux.dato.id, label:aux.dato.dato,}
+            arreglo.push(dato)
+            aux = aux.siguiente
+        }
+        return arreglo
+    }
+
+    //Carga de array para graficacion de enlaces
+    graficarEnlaces(){
+        let aux = this.ListaAdyacencia.cabeza
+        while(aux!=null){
+            let tmp = aux.dato.enlaces.cabeza
+            while(tmp!= null){
+                let egde = {from: tmp.inicio.dato.id, to: tmp.final.dato.id , label: tmp.distancia}
+                arregloEdge.push(egde)
+                tmp = tmp.siguiente
+            }
+            aux = aux.siguiente
+        }
+        return arregloEdge
+    } 
 
 
 
