1495b996f23e9ceaede673e22435cef6fa874234
Erick Mayorga <emayorga30@gmail.com>
2021-07-02 18:09:14 -0600

    Iniciar Diccionario

--- 20211SVAC/G03/src/estructuras/AlgoritmosDeCodificacion/LZW.js
+++ 20211SVAC/G03/src/estructuras/AlgoritmosDeCodificacion/LZW.js
@@ -10,4 +10,24 @@ class LZW {
 
 
 
+
+// inicializar el diccionario con los valores
+    iniciarDiccionario(mensaje) {
+        for (let i = 0; i < mensaje.length; i++) {
+            let repetido = false;
+            for (let letra of this.diccionario.keys()) {
+                if (mensaje.charAt(i) == letra) {
+                    repetido = true;
+                }
+            }
+            if (repetido == false) {
+                this.diccionario.set(mensaje.charAt(i), this.contador);
+                this.contador++;
+            }
+
+
+        }
+
+    }
+
 }
