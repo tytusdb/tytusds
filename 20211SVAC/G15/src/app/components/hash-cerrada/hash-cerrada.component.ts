ec08eb2d2b2bfa6cb2842de155eb8164d467094e
Javi1427 <javierrecinos27@gmail.com>
2021-07-02 21:52:36 -0600

    Edicion y eliminacion de datos

--- 20211SVAC/G15/src/app/components/hash-cerrada/hash-cerrada.component.ts
+++ 20211SVAC/G15/src/app/components/hash-cerrada/hash-cerrada.component.ts
@@ -1,52 +1,136 @@
 import { Component, OnInit } from '@angular/core';
-import {TablaHash,Funcion,Prueba} from '../../helpers/TablaHashCerrada/TablaHash'
+import { TablaHash, Funcion, Prueba } from '../../helpers/TablaHashCerrada/TablaHash'
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-hash-cerrada',
   templateUrl: './hash-cerrada.component.html',
   styleUrls: ['./hash-cerrada.component.css']
 })
 export class HashCerradaComponent implements OnInit {
-  numero:number|string = ''
+  numero: number | string = ''
   fileName
-  tamanio:number = 3
-  funcionHash:number = 1
-  pruebaHash:number = 1
+  tamanio: number = 3
+  funcionHash: number = 1
+  pruebaHash: number = 1
 
-  maxHash:number = 60 
-  minHash:number = 50
+  maxHash: number = 60
+  minHash: number = 50
 
-  flagCreada:boolean = false
+  flagCreada: boolean = false
 
   tablaHash: TablaHash
-  constructor() { }
+
+  values: any[]
+
+  duracion: number = 0.3
+
+  ocupacion: number = 0
+
+  numeroBuscar: number | string = 0
+
+  numeroEliminar: number | string = 0
+
+  numeroAntiguo: number | string = 0
+  numeroNuevo: number | string = 0
+  constructor() {
+    this.values = []
+  }
 
   ngOnInit(): void {
   }
 
-  add(){
-    if(!this.flagCreada){
-      this.tablaHash = new TablaHash(this.tamanio,this.getFuncion(),this.getPrueba(),this.minHash,this.maxHash)
+  async add() {
+    if (!this.flagCreada) {
+      document.getElementById('consola').innerHTML = "Creando Tabla Hash"
+      this.tablaHash = new TablaHash(this.tamanio, this.getFuncion(), this.getPrueba(), this.minHash, this.maxHash, this.values)
       this.flagCreada = true
+
     }
 
-    this.tablaHash.add(this.numero)
+    await this.tablaHash.add(this.numero, this.duracion)
     let ocupacion = this.tablaHash.obtenerOcupacion()
-    if(ocupacion >= this.minHash){
-      this.tablaHash.reHashing()
+    this.ocupacion = ocupacion
+    if (ocupacion >= this.minHash) {
+      document.getElementById('consola').innerHTML += `<br />----------------Haciendo ReHashing-------------`
+      await this.tablaHash.reHashing(this.duracion)
+      this.tamanio = this.values.length
+      this.ocupacion = this.tablaHash.obtenerOcupacion()
     }
     this.numero = ''
   }
 
 
-  getFuncion():Funcion{
-    if(+this.funcionHash === 1) return Funcion.SIMPLE 
-    else if(+this.funcionHash === 2) return Funcion.MULTIPLICACION 
+  async search() {
+    let result = await this.tablaHash.search(this.numeroBuscar, this.duracion)
+    if (result !== null) {
+      Swal.fire({
+        icon: 'success',
+        title: ':)',
+        text: `Se econtro el numero ${this.numeroBuscar} en la posicion ${result.index} `
+      })
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: ':(',
+        text: `No se econtro el numero ${this.numeroBuscar}  `
+      })
+    }
+    this.numeroBuscar = ''
+  }
+
+
+  async delete() {
+    let result = await this.tablaHash.delete(this.numeroEliminar, this.duracion)
+    if (result !== null) {
+      Swal.fire({
+        icon: 'success',
+        title: ':)',
+        text: `Se Elimino el numero ${this.numeroEliminar} en la posicion ${result.index} `
+      })
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: ':(',
+        text: `No se econtro el numero ${this.numeroEliminar}  `
+      })
+    }
+    this.numeroEliminar = ''
+  }
+
+
+  async edit() {
+    let result = await this.tablaHash.edit(this.numeroAntiguo,this.numeroNuevo, this.duracion)
+    if (result !== null) {
+      Swal.fire({
+        icon: 'success',
+        title: ':)',
+        text: `Se Edito el numero ${this.numeroAntiguo} por ${this.numeroNuevo} `
+      })
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: ':(',
+        text: `No se econtro el numero ${this.numeroAntiguo}  `
+      })
+    }
+    this.numeroAntiguo = ''
+    this.numeroNuevo = ''
+  }
+
+
+
+  getFuncion(): Funcion {
+    if (+this.funcionHash === 1) return Funcion.SIMPLE
+    else if (+this.funcionHash === 2) return Funcion.MULTIPLICACION
     return Funcion.DIVISION
   }
 
-  getPrueba():Prueba{
-    if(+this.pruebaHash === 1) return Prueba.LINEAL
-    else if(+this.pruebaHash === 2) return Prueba.CUADRATICA 
+  getPrueba(): Prueba {
+    if (+this.pruebaHash === 1) return Prueba.LINEAL
+    else if (+this.pruebaHash === 2) return Prueba.CUADRATICA
     return Prueba.DOBLE
   }
 
--- 20211SVAC/G15/src/app/helpers/TablaHashCerrada/TablaHash.ts
+++ 20211SVAC/G15/src/app/helpers/TablaHashCerrada/TablaHash.ts
@@ -11,7 +11,7 @@ export enum Prueba {
 }
 
 export class TablaHash {
-    private values: any = []
+    private values: any[]
     private size: number
     private funcion: Funcion
     private constante: number
@@ -20,11 +20,11 @@ export class TablaHash {
     private max: number
     private primos: any[]
 
-    constructor(size: number, funcion: Funcion, prueba: Prueba, min: number, max: number) {
+    constructor(size: number, funcion: Funcion, prueba: Prueba, min: number, max: number, values: any[]) {
         this.size = size
         this.funcion = funcion
         this.prueba = prueba
-        this.values = []
+        this.values = values
         this.constante = 0.6
         this.min = min
         this.max = max
@@ -33,26 +33,61 @@ export class TablaHash {
         this.llenarPrimos()
     }
 
-    public add(value) {
+    public async add(value, duracion) {
         let hash = 0
         if (this.funcion === Funcion.SIMPLE || this.funcion === Funcion.DIVISION) hash = this.funcionSimple(value)
         else if (this.funcion === Funcion.MULTIPLICACION) hash = this.funcionMultiplicacion(value)
 
+
         let dato = this.values[hash]
-        if (dato.libre === true) {
+        if (dato.libre === true || dato.libre === "borrado") {
+            await this.sleep(duracion * 1000)
             this.values[hash] = { value: value, libre: false }
-            console.log("directo", this.values)
+            document.getElementById('consola').innerHTML += `<br />Valor ${value} insertado en posicion: ${hash}`
+            //console.log("directo", this.values)
             return 1
         }
 
         let r = 0
+        await this.sleep(duracion * 1000)
         if (this.prueba === Prueba.LINEAL) r = this.pruebaLineal(value, hash)
         else if (this.prueba === Prueba.CUADRATICA) r = this.funcionCuadratica(value, hash)
         else r = this.funcionDobleHashing(value, hash)
-        console.log(this.values)
+        //console.log(this.values)
         return r
     }
 
+    public async search(value, duracion) {
+        let hash = 0
+        if (this.funcion === Funcion.SIMPLE || this.funcion === Funcion.DIVISION) hash = this.funcionSimple(value)
+        else if (this.funcion === Funcion.MULTIPLICACION) hash = this.funcionMultiplicacion(value)
+
+        if (this.getValue(this.values[hash].value) === this.getValue(value)) {
+            await this.sleep(duracion * 1000)
+            return { index: hash, value: value }
+        }
+        await this.sleep(duracion * 1000)
+        if (this.values[hash].libre === true) return null
+        if (this.prueba === Prueba.LINEAL) return this.busquedaLineal(value, hash)
+        else if (this.prueba === Prueba.CUADRATICA) return this.busquedaCuadratica(value, hash)
+        else return this.busquedaDobleHashing(value, hash)
+
+    }
+
+    public async edit(antiguo,nuevo,duracion){
+        let resultado = await this.delete(antiguo,duracion)
+        if(resultado === null) return null 
+        return this.add(nuevo,duracion)
+    }
+
+
+    public async delete(value,duracion){
+        let resultado = await this.search(value,duracion)
+        if(resultado === null) return null 
+        document.getElementById('consola').innerHTML += `<br />Se elimino el valor ${value} antigua Posicion: ${resultado.index}`
+        this.values[resultado.index] = {value:0,libre:"borrado"}
+        return resultado
+    }
 
     private funcionSimple(value): number {
         return this.getValue(value) % this.size
@@ -68,6 +103,7 @@ export class TablaHash {
             if (i + hash >= this.size) return -1
             let dato = this.values[(i * i) + hash]
             if (dato.libre || dato.libre === "borrado") {
+                document.getElementById('consola').innerHTML += `<br />Valor ${value} insertado en posicion: ${i + hash}`
                 this.values[(i * i) + hash] = {
                     value: value,
                     libre: false
@@ -84,9 +120,9 @@ export class TablaHash {
 
         for (let i = 1; i < 5; i++) {
             let newHash = (hash + (i * hash2)) % this.size
-            console.log("new Hash: ", newHash)
             let dato = this.values[newHash]
             if (dato.libre || dato.libre === "borrado") {
+                document.getElementById('consola').innerHTML += `<br />Valor ${value} insertado en posicion: ${i}`
                 this.values[newHash] = {
                     value: value,
                     libre: false
@@ -117,6 +153,7 @@ export class TablaHash {
         for (let i = index; i < this.size; i++) {
             let dato = this.values[i]
             if (dato.libre || dato.libre === "borrado") {
+                document.getElementById('consola').innerHTML += `<br />Valor ${value} insertado en posicion: ${i}`
                 this.values[i] = {
                     value: value,
                     libre: false
@@ -127,8 +164,44 @@ export class TablaHash {
         return 0
     }
 
+    private busquedaLineal(value, hash) {
+        for (let i = hash; i < this.size; i++) {
+            if (this.getValue(this.values[i].value) === this.getValue(value)) return { index: i, value: value }
+        }
+
+        return null
+    }
+
+    private busquedaCuadratica(value, hash) {
+        let i = 1
+        while (true) {
+            if (i + hash >= this.size) return null
+            let dato = this.values[(i * i) + hash]
+            if (this.getValue(dato.value) === this.getValue(value)) return { index: i + hash, value: value }
+            i++
+        }
+        return null
+    }
+
+
+    private busquedaDobleHashing(value, hash) {
+        let hash2 = this.hashing2(value)
+
+        for (let i = 1; i < 5; i++) {
+            let newHash = (hash + (i * hash2)) % this.size
+            let dato = this.values[newHash]
+            if(this.getValue(dato.value) === this.getValue(value)) return {index:newHash,value:value}
+        }
+
+        return null
+    }
+
+
     private llenarMatriz() {
-        for (let i = 0; i < this.size; i++) this.values.push({ value: 0, libre: true })
+        for (let i = 0; i < this.size; i++) {
+            if (this.values[i]) this.values[i] = { value: 0, libre: true }
+            else this.values.push({ value: 0, libre: true })
+        }
     }
 
 
@@ -164,34 +237,36 @@ export class TablaHash {
         return numero !== 1;
     }
 
-    private obtenerPrimo(size):number{
-        for(let i = 0; i < this.primos.length; i++){
-            if(this.primos[i] >= size) return this.primos[i]
+    private obtenerPrimo(size): number {
+        for (let i = 0; i < this.primos.length; i++) {
+            if (this.primos[i] >= size) return this.primos[i]
         }
         return 1000
     }
 
-    public reHashing(){
+    public async reHashing(duracion) {
+        //console.log(this.values)
         let newSize = this.size * 2
         newSize = this.obtenerPrimo(newSize)
+        document.getElementById('consola').innerHTML += `<br />Nuevo Tamaño: ${newSize}`
         let clone = this.clone(this.values)
-        this.size = newSize 
-        this.values = []
+        this.size = newSize
         this.llenarMatriz()
-        
-        for(let i = 0; i < clone.length; i++){
-            this.add(this.getValue(clone[i].value))
+        document.getElementById('consola').innerHTML += `<br />Insertando Valores Nuevamente`
+        for (let i = 0; i < clone.length; i++) {
+            if (!(clone[i].libre === true)) await this.add(this.getValue(clone[i].value), duracion)
         }
-        
-        
+        document.getElementById('consola').innerHTML += `<br />----------------Fin ReHashing---------------`
+
+
     }
 
-    private clone(arreglo){
+    private clone(arreglo) {
         let arr = []
-        for(let i = 0; i < arreglo.length; i++){
+        for (let i = 0; i < arreglo.length; i++) {
             arr.push(
                 {
-                    value : arreglo[i].value, 
+                    value: arreglo[i].value,
                     libre: arreglo[i].libre
                 }
             )
@@ -200,4 +275,8 @@ export class TablaHash {
         return arr
     }
 
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 }
