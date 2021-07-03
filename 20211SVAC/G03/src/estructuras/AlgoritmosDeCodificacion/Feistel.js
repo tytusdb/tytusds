2fe94463d19c1c640f038344d1b3dafbd8cb0442
Erick Mayorga <emayorga30@gmail.com>
2021-07-02 18:06:51 -0600

    CodificarBinario

--- 20211SVAC/G03/src/estructuras/AlgoritmosDeCodificacion/Feistel.js
+++ 20211SVAC/G03/src/estructuras/AlgoritmosDeCodificacion/Feistel.js
@@ -6,6 +6,23 @@ class Feistel{
         this.tablaDerecha = [];   
     }
 
+    codificarBinario(datos, llave, numeroPasadas){
+        let left = "";
+        let right = "";
+        for(let i = 0; i < datos.length; i++){
+            if(i< (datos.length/2)){
+                left += datos.charAt(i)                
+            }else if(i>= (datos.length/2)){
+                right += datos.charAt(i)
+            }
+        } 
+        this.tablaIzquierda.push(left);
+        this.tablaDerecha.push(right); 
+        this.ciclosCodificar(left, right,llave, numeroPasadas);
+        this.imprimirTablas();
+
+
+    }
 
 
 
