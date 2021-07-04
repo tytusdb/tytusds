316a4d9849d1d6ab85679238ae1c40d879b0bfaf
Victor Cuches de León <69135983+VictorCuches@users.noreply.github.com>
2021-07-04 02:10:13 -0600

    Implementacion de Row-Major (pendiente)

--- 20211SVAC/G08/js/compuestas/row.js
+++ 20211SVAC/G08/js/compuestas/row.js
@@ -1,21 +1,29 @@
 
 // E  V  E  N  T  O  S
-/*
-// ----- OBTENER LLAVE -----
-var btn_Key = document.getElementById("addLlave");
-btn_Key.addEventListener("click", saveKey);
 
-// ----- OBTENER NUM DE RONDAS -----
-var btn_Rondas = document.getElementById("addRonda");
-btn_Rondas.addEventListener("click", rondaInfo);
-*/
+// ----- VELOCIDAD -----
+var btn_Valor = document.getElementById("addValor");
+btn_Valor.addEventListener("click", insertarV)
+
+// ----- CONSTRUIR TABLA -----
+var btn_Tabla = document.getElementById("construir");
+btn_Tabla.addEventListener("click", infoTabla);
+
+// ----- ELIMINAR ELEMENTO -----
+var btn_Eliminar = document.getElementById("eliminar");
+btn_Eliminar.addEventListener("click", eliminar);
+
+// ----- BUSCAR ELEMENTO -----
+var btn_Buscar = document.getElementById("buscar");
+btn_Buscar.addEventListener("click", buscar);
+
+// ----- ACTUALIZAR ELEMENTO -----
+var btn_Actualizar = document.getElementById("actualizar");
+btn_Actualizar.addEventListener("click", actualizar);
+
 // ----- VELOCIDAD -----
 var btn_Velocidad = document.getElementById("velocidad");
-btn_Velocidad.addEventListener("click", getVelocidad)
-
-// ----- CODIFICAR -----
-var btn_Code = document.getElementById("code");
-btn_Code.addEventListener("click", codificar);
+btn_Velocidad.addEventListener("click", getVelocidad);
 
 // ----- GUARDAR ARCHIVO -----
 var btn_Save = document.getElementById("guardar");
@@ -25,212 +33,147 @@ btn_Save.addEventListener("click", guardar);
 var btn_Clear = document.getElementById("limpiar");
 btn_Clear.addEventListener("click", limpiar)
 
-// ----- TEXTAREA -----
-var spc_Text = document.getElementById("espacioTxt");
+// ----- LIMPIAR PANTALLA ----- 
+var btn_RowMaj = document.getElementById("rowMaj");
+btn_RowMaj.addEventListener("click", rowMaj)
 
-// ----- CUADROS PARA ANIMACION -----
-var izqui = document.getElementById("left");
-var dere = document.getElementById("right");
-var func = document.getElementById("func");
-var llavero = document.getElementById("llavero");
-var resultado = document.getElementById("respuesta");
 
 // V  A  R  I  A  B  L  E  S  -  G  L  O  B  A  L  E  S
-var contenido;
-var rondas;
+var fil;
+var col; 
 var velocidad;
+var conTabla = [];
 
 
 // F  U  N  C  I  O  N  E  S  -  E  V  E  N  T  O  S
-// ****** LLAVE *****
-function saveKey(){
-    key = document.getElementById("llave").value;
+// ***** INSERTAR VALOR EN TABLA *****
+function insertarV(){
+    var value = document.getElementById("valor").value;
+    var x = parseInt(document.getElementById("fila").value);
+    var y = parseInt(document.getElementById("colum").value);
+    console.log("Insertando: "+value+" en fila: "+x+" columna: "+y);
+    conTabla[x][y] = value;
+    buildTabla();
 
-}
-// ****** NUMERO DE RONDAS *****
-function rondaInfo(){
-    rondas = document.getElementById("rondas").value;
-    console.log("Numero de rondas: "+rondas);
 
+
+   
 }
-// ***** CODIFICAR TEXTO *****
-function codificar(){
-    let leftSide = [];
-    let rightSide = [];
-    console.log("Codificando");
-    
-    // obteniendo contenido del textarea por si se modifica algo
-    var texto = document.getElementById("espacioTxt").value;
-    var key = document.getElementById("llave").value;
-    rondas = parseInt(document.getElementById("rondas").value);
-    
-    console.log(texto.length)
-    // LA LLAVE SERA POR EL MOMENTO IGUAL A 1 - PENDIENTE DE CAMBIOS
-    var llave = llaveBin(convertSB(key).split(""));
-    var charBin = convertSB(texto); // lista de caracteres en binario
-    console.log(charBin+" - "+llave);
-    console.log("rondas: "+parseInt(rondas))
-    
-    // partir la cadena en dos 
-    let pivote = Math.floor(charBin.length / 2);
-    for (let i = 0; i < charBin.length; i++){
-        if(i < pivote){
-            leftSide.push(parseInt(charBin[i]));
-        } else {
-            rightSide.push(parseInt(charBin[i]));
-        }
+
+// ***** INFO TABLA *****
+function infoTabla(){
+    fil = parseInt(document.getElementById("fila").value);
+    col = parseInt(document.getElementById("colum").value);
+    console.log("fila: "+fil+" columna: "+col);
+   
+    conTabla = new Array(fil)
+    for(let i = 0; i < fil; i++){
+        conTabla[i] = new Array(col)
     }
-    if(leftSide.length > llave.length){
 
-        llave = addZero(llave, leftSide.length);
+    // ahora que tengo los datos tengo que construir la tabla
+    buildTabla();
 
-    }
-    console.log(leftSide);
-    console.log(rightSide);
-    console.log(llave);
-    console.log("Aplicando Feistel")
-    feistel(leftSide, rightSide, llave);
-    
-    izqui.textContent =leftSide.join("");
-    dere.textContent =rightSide.join("");
-    llavero.textContent = llave.join("");
-    /*
-    await new Promise((resolve) =>
-                setTimeout(() =>{
-                    resolve();
-                }, (velocidad*200)) //delay
-            );
-    */
+
 }
-
-// ***** CIFRADO FEISTEL *****
-async function feistel(L, R, key){
-    let contador = 0;
-    let der = [];
-    let temp = [];
+// ***** CONSTRUIR TABLA *****
+function buildTabla(){
     velocidad = 10;
-    while(contador < parseInt(rondas)){
-        console.log("ITERACION W"+contador)
+    var cuadroTabla = document.getElementById("espacio");
+    var control = false;
+    cuadroTabla.innerHTML = "";
+    var tab = "<table class=\"tablero\">";
+    for (let i = 0; i < fil; i++){
+        tab += "<tr>"
+        for (let j = 0; j < col; j++){
+
+            if(conTabla[i][j] != null){
+                console.log("entre")
+                tab += "<td>"+conTabla[i][j]+"</td>";
+                
+            } else {
+                tab += "<td>"+" "+"</td>";
+
+
+            }
+
+            
+        }
         
-        dere.textContent = R.join("");
-        llavero.textContent = key.join("");
-        dere.style.backgroundColor = "#FF4949";
-        llavero.style.backgroundColor = "#FF4949";
-
-        await new Promise((resolve) =>
-        setTimeout(() =>{
-            resolve();
-        }, (velocidad*200)) //delay
-        );
-
-        for(let i = 0; i < R.length; i++){
-            temp.push(R[i] ^ key[i])
-        } //01110111 //01010110
-        
-        func.style.backgroundColor = "#29C1F5";
-        func.textContent = temp.join("");
-        await new Promise((resolve) =>
-        setTimeout(() =>{
-            resolve();
-        }, (velocidad*200)) //delay
-        );
-        dere.style.backgroundColor = "#FFFFFF";
-        llavero.style.backgroundColor = "#FFFFFF";
-        func.style.backgroundColor = "#FF4949";
-        izqui.style.backgroundColor = "#FF4949";
-        await new Promise((resolve) =>
-        setTimeout(() =>{
-            resolve();
-        }, (velocidad*200)) //delay
-        );
-        console.log(temp);
-        for(let j = 0; j < L.length; j++){
-            der.push(L[j] ^ temp[j])
-        } //00110100
-        dere.style.backgroundColor = "#29C1F5";
-        dere.textContent = der.join("");
-        await new Promise((resolve) =>
-        setTimeout(() =>{
-            resolve();
-        }, (velocidad*200)) //delay
-        );
-        func.style.backgroundColor = "#FFFFFF";
-        izqui.style.backgroundColor = "#FFFFFF";
-        dere.style.backgroundColor = "#FFFFFF";
-        console.log(der);
-        L = R; //01000110
-        console.log(L);
-        R = der; //00110100
-        console.log(R);
-        temp = [];
-        der = [];
-
-        let ultimo = key.shift();
-        key.push(ultimo);
-        
-        console.log(key);
-        izqui.style.backgroundColor = "#E2F240";
-        izqui.textContent = L.join("");
-        llavero.style.backgroundColor = "#E2F240";
-        llavero.textContent = key.join("");
-        await new Promise((resolve) =>
-        setTimeout(() =>{
-            resolve();
-        }, (velocidad*200)) //delay
-        );
-        izqui.style.backgroundColor = "#FFFFFF";
-        llavero.style.backgroundColor = "#FFFFFF";
-
-      
-        contador++;
+        tab += "</tr>"
     }
-    console.log(L.join("")+"  |  "+R.join(""))
-    console.log("RESULTADO FINAL")
-    console.log(R);
-    let allBin = L.join("") + R.join("");
-    var codeF = convertBS(allBin);
-    console.log(convertBS(allBin));
-    resultado.textContent = codeF;
-
+    tab += "</table>";
+    cuadroTabla.innerHTML = tab;
 }
 
-// ***** CONVERTIR EL CONTENIDO A BINARIO *****
-function convertSB(text) {
-    var length = text.length, output = [];
-    for (var i = 0;i < length; i++) {
-      var bin = text[i].charCodeAt().toString(2);
-      output.push(Array(8-bin.length+1).join("0") + bin);
-    } 
-    return output.join("");
+// ***** ROW-MAJOR *****
+function rowMaj(){
+    console.log("Realizando Row-Major");
+   
 }
 
-function convertBS(str) {
-    // Removes the spaces from the binary string
-    str = str.replace(/\s+/g, '');
-    // Pretty (correct) print binary (add a space every 8 characters)
-    str = str.match(/.{1,8}/g).join(" ");
+// ***** ELIMINAR ELEMENTO *****
+function eliminar(){
+    console.log("Eliminando");
+    var value = document.getElementById("valor").value;
+    var x = parseInt(document.getElementById("fila").value);
+    var y = parseInt(document.getElementById("colum").value);
+    console.log("Insertando: "+value+" en fila: "+x+" columna: "+y);
+    conTabla[x][y] = null;
+    buildTabla();
 
-    return str.split(" ").map(function (elem) {
-        return String.fromCharCode(parseInt(elem, 2));
-    }).join("");
+   
 }
 
-// ***** CAMBIAR LLAVE *****
-function llaveBin(content){
-    let newList = [];
-    for(let i = 0; i < content.length; i++){
-        newList.push(parseInt(content[i]))
+// ***** BUSCAR ELEMENTO *****
+function buscar(){
+    console.log("Buscando");
+    var buscado = document.getElementById("valor").value;
+    var x = parseInt(document.getElementById("fila").value);
+    var y = parseInt(document.getElementById("colum").value);
+    findTabla(buscado);
+   
+}
+// ***** CONSTRUIR TABLA  AL BUSCAR*****
+function findTabla(buscado){
+    velocidad = 10;
+    var cuadroTabla = document.getElementById("espacio");
+    var control = false;
+    cuadroTabla.innerHTML = "";
+    var tab = "<table class=\"tablero\">";
+    for (let i = 0; i < fil; i++){
+        tab += "<tr>"
+        for (let j = 0; j < col; j++){
+
+            if(conTabla[i][j] != null){
+                if(conTabla[i][j] == buscado){
+                    tab += "<td class =\"buscado\">"+conTabla[i][j]+"</td>";
+
+                } else {
+                    tab += "<td>"+conTabla[i][j]+"</td>";
+                }
+            } else {
+                tab += "<td>"+" "+"</td>";
+            }    
+        } 
+        tab += "</tr>"
     }
-    return newList;
+    tab += "</table>";
+    cuadroTabla.innerHTML = tab;
 }
 
-// ***** EMPAREJAR LLAVE *****
-function addZero(lKey, size){
-    while(lKey.length < size){
-        lKey.unshift(0);
-    }
-    return lKey;
+// ***** ACTUALIZAR ELEMENTO *****
+function actualizar(){
+    console.log("Actualizando");
+    var value = document.getElementById("valor").value;
+    var x = parseInt(document.getElementById("fila").value);
+    var y = parseInt(document.getElementById("colum").value);
+    console.log("Actualizando: "+value+" en fila: "+x+" columna: "+y);
+    conTabla[x][y] = value;
+    buildTabla();
+   
 }
+
 // ***** LIMPIAR PANTALLA *****
 function limpiar(){
     console.log("Limpiando");
@@ -252,6 +195,7 @@ function guardar(){
 }
 // ***** LEYENDO ARCHIVO *****
 function readFile(evento){ // lectura del archivo .json
+    velocidad = 10;
     let archivo = evento.target.files[0];
     if (archivo){
         let reader = new FileReader();
@@ -260,7 +204,45 @@ function readFile(evento){ // lectura del archivo .json
             // console.log(contenido)
             console.log("-----------")
             console.log(contenido);
-            spc_Text.textContent = contenido;
+            convert = JSON.parse(contenido);
+            console.log(convert.m)
+            // construyendo tabla con las dimensiones del archivo
+            fil = convert.m[0]; // FILAS
+            col = convert.m[1]; // COLUMNAS
+            
+
+            //obteniendo datos y enviando uno por uno a la tabla
+            var valCelda = convert.valores;
+            console.log(valCelda.length); 
+            /*
+
+         
+            for (let i = 0; i < valCelda.length; i++){
+                conTabla.push({x:valCelda[i].indices[0], y : valCelda[i].indices[1], valor : valCelda[i].valor  })
+               
+            }
+            console.log("Lista -----")
+            console.log(conTabla)
+            console.log(conTabla[0])
+            */
+
+            conTabla = new Array(fil)
+            for(let i = 0; i < fil; i++){
+                conTabla[i] = new Array(col)
+            }
+            //Ahora metemos los valores dentro de la matriz
+            for(let i = 0; i < valCelda.length; i++){
+                let x = valCelda[i].indices[0]
+                let y = valCelda[i].indices[1]
+                let valor = valCelda[i].valor
+                conTabla[x][y] = valor
+            }
+            console.log(conTabla)
+            
+            //repeat();
+            buildTabla();
+            
+            
 
         };
         reader.readAsText(archivo); 
@@ -273,3 +255,21 @@ function readFile(evento){ // lectura del archivo .json
 window.addEventListener('load', ()=>{ // cada vez que cambie 
     document.getElementById('file').addEventListener('change',readFile)
 });
+
+// ***** REPETIDOS *****
+function repeat(){
+    var inds = []
+    for (let i= 0; i < conTabla.length; i++) {
+        for (let j = 0; j < conTabla.length; j++) {
+            if ((conTabla[i].x == conTabla[j].x) && (conTabla[i].y == conTabla[j].y) && (conTabla[i].valor != conTabla[j].valor)){
+                inds.push(i);
+                
+                console.log("Encontre un repetido i: "+i+" j:"+j+" Valor i:"+conTabla[i].valor+" Valor j: "+conTabla[j].valor )
+                conTabla.replace(conTabla[i],conTabla[j]);
+                conTabla.splice(j,1);
+            }    
+        }
+    }
+    
+    console.log(conTabla)
+}
