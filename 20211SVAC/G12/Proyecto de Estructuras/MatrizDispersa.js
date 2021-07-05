8fb707555b8093bd3f90bb2b809c0300ecafa5d8
Bryan <hernandezdelcidbryan@gmail.com>
2021-07-05 14:17:39 -0600

    "Sparse matrix RowMayor demo"

--- 20211SVAC/G12/Proyecto de Estructuras/MatrizDispersa.js
+++ 20211SVAC/G12/Proyecto de Estructuras/MatrizDispersa.js
@@ -1,3 +1,14 @@
+class File {
+    constructor(categoria,nombre,animacion,valores) {
+        this.categoria=categoria
+        this.nombre=nombre
+        this.almacenamiento=animacion
+        this.valores=valores
+    }
+}
+var file;
+var speed;
+
 class Nodo {
     constructor(val,x,y) {
         this.val=val
@@ -12,69 +23,72 @@ class Nodo {
 
 class Matriz {
     constructor() {
-        this.head = new Nodo(0,0,0);
+        this.head = new Nodo(-1,-1,-1);
+        this.arreglo=[];
+        this.rows=0;
+        this.columns=0;
+        this.vals=0
     }
 
     addColumn(val) {
-        let temp
-        temp = this.head.right
-        if (temp == null) {
-            let nw = new Nodo(val, val, 0);
-            this.head.right = nw;
-            nw.left = this.head
+        this.columns++
+        let y=this.head
+        let aux = new Nodo(val, val, -1)
+        if (y.right == null) {
+            y.right = aux;
+            aux.left = y;
         } else {
-            while (temp.right != null && temp.x < val) {
-                temp = temp.right;
+            let te = y.right;
+            while (te.right != null && te.x < aux.x) {
+                te = te.right;
             }
-            if (temp.right == null && temp.x < val) {
-                let nw = new Nodo(val, val, 0);
-                temp.right = nw;
-                nw.left = temp;
-            } else if (temp.right == null && temp.x > val) {
-                let nw = new Nodo(val, val, 0);
-                temp.left.right = nw;
-                nw.right = temp;
-                nw.left = temp.left;
-                temp.left = nw;
-            } else if (temp.right != null && temp.x != val) {
-                let aux = temp.right;
-                let nw = new Nodo(val, val, 0);
-                temp.right = nw;
-                nw.right = aux;
-                aux.left = nw;
-                nw.left = temp;
+            if (te.right == null) {
+                if(te.x>aux.x){
+                    aux.left=te.left
+                    aux.right=te
+                    te.left.right=aux
+                }else{
+                    te.right = aux;
+                    aux.left = te;
+                }
+            } else if (te.x > aux.x) {
+                let temporal = te.left
+                aux.left=te.left
+                aux.right=te
+                te.left.right=aux
+                te.left=aux
             }
         }
     }
 
     addRow(val) {
-        let temp
-        temp = this.head.down
-        if (temp == null) {
-            let nw = new Nodo(val, 0, val);
-            this.head.down = nw;
-            nw.up = this.head;
+        this.rows++
+        let x=this.head
+        let aux = new Nodo(val, -1, val)
+        if (x.down == null) {
+            x.down = aux;
+            aux.up = x;
         } else {
-            while (temp.down != null && temp.y < val) {
-                temp = temp.down;
+            let te = x.down
+            while (te.down != null && te.y < aux.y) {
+                te = te.down
             }
-            if (temp.down == null && temp.y < val) {
-                let nw = new Nodo(val, 0, val);
-                temp.down = nw;
-                nw.up = temp;
-            } else if (temp.down == null && temp.y > val) {
-                let nw = new Nodo(val, 0, val);
-                temp.up.down = nw;
-                nw.down = temp;
-                nw.up = temp.up;
-                temp.up = nw;
-            } else if (temp.down != null && temp.y != val) {
-                let aux = temp.down;
-                let nw = new Nodo(val, 0, val);
-                temp.down = nw;
-                nw.down = aux;
-                aux.up = nw;
-                nw.up = temp;
+            if (te.down == null) {
+                if(te.y>aux.y){
+                    aux.up=te.up
+                    aux.down=te
+                    te.up.down=aux
+                    te.up=aux
+                }else{
+                    te.down = aux;
+                    aux.up = te;
+                }
+            } else if (te.y > aux.y) {
+                let temporal = te.up
+                aux.up=te.up
+                aux.down=te
+                te.up.down=aux
+                te.up=aux
             }
         }
     }
@@ -111,13 +125,32 @@ class Matriz {
         }
     }
 
+    search(xe,ye){
+        let x=this.getx(parseInt(xe))
+        let y=this.gety(parseInt(ye))
+        if(x!=null&&y!=null){
+            while (x.down!=null&&x.y!=ye){
+                x=x.down
+            }
+            if(x.y==ye){
+                return x
+            }
+            return null
+        }
+        return null
+    }
+
     insert(xe, ye, distancia) {
+        this.vals++
+        let s=this.search(xe,ye)
+        if(s!=null){
+            s.val+="\n"+distancia
+            return
+        }
         xe=parseInt(xe);
         ye=parseInt(ye);
         let x;
         let y;
-        let t;
-        let t2;
         x = this.getx(xe);
         y = this.gety(ye);
         if (x == null) {
@@ -142,16 +175,16 @@ class Matriz {
                     aux.up=te.up
                     aux.down=te
                     te.up.down=aux
+                    te.up=aux
                 }else{
                     te.down = aux;
                     aux.up = te;
                 }
-            } else if (te.y < aux.y) {
-                let temporal = te.down
-                te.down = aux;
-                aux.down = temporal;
-                temporal.up = aux;
-                aux.up = te;
+            } else if (te.y > aux.y) {
+                aux.up=te.up
+                aux.down=te
+                te.up.down=aux
+                te.up=aux
             }
         }
 
@@ -168,16 +201,16 @@ class Matriz {
                     aux.left=te.left
                     aux.right=te
                     te.left.right=aux
+                    te.left=aux
                 }else{
                     te.right = aux;
                     aux.left = te;
                 }
-            } else if (te.x < aux.x) {
-                let temporal = te.right
-                te.right = aux;
-                aux.right = temporal;
-                temporal.left = aux;
-                aux.left = te;
+            } else if (te.x > aux.x) {
+                aux.left=te.left
+                aux.right=te
+                te.left.right=aux
+                te.left=aux
             }
         }
 
@@ -271,14 +304,14 @@ class Matriz {
 
         while (y!=null){
             if(y!=this.head){
-                res+=`<div id="${y.x}-${y.y}" class="nodoH" style="left: ${200}px;top: ${200*(y.y+1)}px">${y.val}</div>`
+                res+=`<div id="${y.x}-${y.y}" class="nodoH" style="left: ${200}px;top: ${200*(y.y+2)}px">${y.val}</div>`
             }
             x=y.right
             while (x!=null){
-                if(x.y==0){
-                    res+=`<div id="${x.x}-${x.y}" class="nodoH" style="left: ${200*(x.x+1)}px;top: 200px">${x.val}</div>`
+                if(x.y==-1){
+                    res+=`<div id="${x.x}-${x.y}" class="nodoH" style="left: ${200*(x.x+2)}px;top: 200px">${x.val}</div>`
                 }else {
-                    res+=`<div id="${x.x}-${x.y}" class="nodo" style="left: ${200*(x.x+1)}px;top: ${200*(x.y+1)}px">${x.val}</div>`
+                    res+=`<div id="${x.x}-${x.y}" class="nodo" style="left: ${200*(x.x+2)}px;top: ${200*(x.y+2)}px">${x.val}</div>`
                 }
                 x=x.right
             }
@@ -289,25 +322,25 @@ class Matriz {
         x=this.head
         while (y!=null){
 
-           if(y!=this.head&&y.down!=null){
-               let id1=y.x+"-"+y.y;
-               let id2=y.down.x+"-"+y.down.y;
-               let a=document.getElementById(id1)
-               let b=document.getElementById(id2)
-               connect(a,b,"Black","10")
-           }
+            if(y!=this.head&&y.down!=null){
+                let id1=y.x+"-"+y.y;
+                let id2=y.down.x+"-"+y.down.y;
+                let a=document.getElementById(id1)
+                let b=document.getElementById(id2)
+                connect(a,b,"Black","10")
+            }
 
             x=y
             while (x!=null){
-                    if(x.right!=null){
-                        let id1=x.x+"-"+x.y;
-                        let id2=x.right.x+"-"+x.right.y;
-                        let a=document.getElementById(id1)
-                        let b=document.getElementById(id2)
-                        if(a!=null&&b!=null){
-                            connect(a,b,"Black","10")
-                        }
+                if(x.right!=null){
+                    let id1=x.x+"-"+x.y;
+                    let id2=x.right.x+"-"+x.right.y;
+                    let a=document.getElementById(id1)
+                    let b=document.getElementById(id2)
+                    if(a!=null&&b!=null){
+                        connect(a,b,"Black","10")
                     }
+                }
                 if(x.down!=null){
                     let id1=x.x+"-"+x.y;
                     let id2=x.down.x+"-"+x.down.y;
@@ -317,6 +350,25 @@ class Matriz {
                         connect(a,b,"Black","10")
                     }
                 }
+                x=x.right
+            }
+            y=y.down
+        }
+
+    }
+
+    async RowMayor(){
+        this.arreglo=[this.rows*this.columns]
+        let y=this.head.down
+        let x=null
+        while (y!=null){
+            x=y.right
+            while (x!=null){
+                let pos=1*(this.columns*(x.x)+(x.y))
+                console.log(pos)
+                this.arreglo[pos]=x
+                let element=document.getElementById(x.x+"-"+x.y)
+                await movexy((pos)*150,90,element,500)
                 x=x.right
             }
             y=y.down
@@ -358,4 +410,35 @@ function connect(div2, div1,color,thickness) { // draw a line connecting element
     var htmlLine = "<div id='l"+id+"' style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left: 0px; top: 30px;   width:" + (length-80) + "px; ' />";
     document.getElementById("result").innerHTML += htmllinecontainer;
     document.getElementById(id).innerHTML += htmlLine;
+}
+
+function movexy(x, y, i, speed) {
+    return new Promise(resolve => {
+        let element = i
+        let posx = element.style.left;
+        let posy = element.style.top;
+        element.animate(
+            [
+                {
+                    left: posx,
+                    top: posy
+                },
+                {
+                    left: x + "px",
+                    top: y + "px"
+                }
+            ],
+            {
+                duration: speed,
+                fill: "forwards"
+            }
+        );
+        setTimeout(() => {
+            element.style.animation = null;
+            element.style.top = y + "px";
+            element.style.left = x + "px";
+
+            resolve()
+        }, speed);
+    });
 }
