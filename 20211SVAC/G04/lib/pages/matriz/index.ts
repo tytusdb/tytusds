cb43f2a27cbd66280a8f5c8270cad6d5fa946adc
rl102306 <2306919780101@ingenieria.usac.edu.gt>
2021-07-04 19:52:45 -0600

    [Add] Set and Get Encabezado
    Crear y obtener el encabezado

--- 20211SVAC/G04/lib/pages/matriz/index.ts
+++ 20211SVAC/G04/lib/pages/matriz/index.ts
@@ -48,3 +48,84 @@ class NodoM {
     }
   
 }
+
+
+
+class listaencabezado {
+
+
+    public primero: any
+
+
+    constructor(primero: any) {
+
+        this.primero = primero
+
+    }
+
+    setEncabezado(nuevo: any) {
+
+        if (this.primero == null) {
+
+            this.primero = nuevo
+
+        } else if (nuevo.id < this.primero.id) {
+
+            nuevo.siguiente = this.primero
+
+            this.primero.anterior = nuevo
+
+            this.primero = nuevo
+
+        } else {
+
+            let actual = this.primero
+
+            while (actual.siguiente != null) {
+
+                if (nuevo.id < actual.siguiente.id) {
+
+                    nuevo.siguiente = actual.siguiente
+
+                    actual.siguiente.anterior = nuevo
+
+                    nuevo.anterior = actual
+
+                    actual = nuevo
+
+                    break
+                }
+
+                actual = actual.siguiente
+
+
+                if (actual.siguiente == null) {
+
+                    actual.siguiente = nuevo
+
+                    nuevo.anterior = actual
+                }
+            }
+        }
+    }
+
+    getEncabezado(id: any) {
+
+        let actual = this.primero
+
+        while (actual != null) {
+
+            if (actual.id == id) {
+
+                return actual
+
+            }
+
+            actual = actual.siguiente
+        }
+
+        return null
+    }
+
+}
+
